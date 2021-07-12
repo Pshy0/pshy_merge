4eb723ec21f28b57e2af668b8b89177841df4a37
redacted <redacted>
2021-07-12 21:15:49 +0200

    removed pshy_misc_utils.lua

--- modules/pshy/pshy_commands/pshy_commands.lua
+++ modules/pshy/pshy_commands/pshy_commands.lua
@@ -122,6 +122,22 @@ end
 
 
 
+--- Convert string arguments of a table to the specified types, 
+-- or attempt to guess the types.
+-- @param args Table of elements to convert.
+-- @param types Table of types.
+function pshy.TableStringsToType(args, types)
+	for index = 1, #args do
+		if types and index <= #types then
+			args[index] = pshy.ToType(args[index], types[index])
+		else
+			args[index] = pshy.AutoType(args[index])
+		end
+	end	
+end
+
+
+
 --- Run a command as a player
 -- @param user The player inputing the command.
 -- @param command The full command the player have input.
