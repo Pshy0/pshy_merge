edec56f9c302063355d2c059ca618bf98a4825a6
redacted <redacted>
2021-07-12 17:07:11 +0200

    added pshy_checkpoints to the fun modulepack

--- modules/modulepack_pshyfun.lua
+++ modules/modulepack_pshyfun.lua
@@ -10,11 +10,14 @@
 -- @require pshy_lua_commands.lua
 -- @require pshy_fun_commands.lua
 -- @require pshy_fcplatform.lua
+-- @require pshy_nicks.lua
+-- @require pshy_checkpoints.lua
+-- @require pshy_motd.lua
 -- @require pshy_weather.lua
 -- @require pshy_basic_weathers.lua
--- @require pshy_motd.lua
--- @require pshy_nicks.lua
 -- @require pshy_rotations.lua
+
+
 
 tfm.exec.disableWatchCommand(false)
 tfm.exec.disableDebugCommand(true)
