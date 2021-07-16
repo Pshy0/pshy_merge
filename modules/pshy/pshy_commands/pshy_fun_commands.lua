ed81ebdbc8a8f4916deefa12b6f9ddf1f0ba214b
redacted <redacted>
2021-07-16 17:12:20 +0200

    cleaning, supporting partial player names

--- modules/pshy/pshy_commands/pshy_fun_commands.lua
+++ modules/pshy/pshy_commands/pshy_fun_commands.lua
@@ -22,19 +22,35 @@ pshy.fun_commands_link_wishes = {}	-- map of player names requiring a link to an
 pshy.fun_commands_flyers = {}		-- flying players
 pshy.fun_commands_speedies = {}	-- speedy players
 
+--pshy.FindPlayerNameOrError(partial_name)
+
+
+--- Get the target of the command, throwing on permission issue
+-- @private
+function pshy.fun_commands_GetTarget(user, target, perm_prefix)
+	assert(type(perm_prefix) == "string")
+	if not target then
+		return user
+	end
+	target = pshy.FindPlayerNameOrError(target)
+	if target == user then
+		return user
+	elseif not pshy.HavePerm(user, perm_prefix .. "-others") then
+		error("you cant use this command on other players :c")
+		return
+	elseif not tfm.get.room.playerList[target] then
+		error("the target player is not in the room")
+		return
+	end
+	return target
+end
+
+
 
 
 --- !shaman
 function pshy.ChatCommandShaman(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!shaman-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!shaman")
 	tfm.exec.setShaman(target, not tfm.get.room.playerList[target].isShaman)
 end
 pshy.chat_commands["shaman"] = {func = pshy.ChatCommandShaman, desc = "switch you to a shaman", argc_min = 0, argc_max = 1, arg_types = {"string"}}
@@ -45,15 +61,7 @@ pshy.perms.everyone["!shaman"] = false
 
 --- !vampire
 function pshy.ChatCommandVampire(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!vampire-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!vampire")
 	tfm.exec.setVampirePlayer(target, not tfm.get.room.playerList[target].isVampire)
 end
 pshy.chat_commands["vampire"] = {func = pshy.ChatCommandVampire, desc = "switch you to a vampire", argc_min = 0, argc_max = 1, arg_types = {"string"}}
@@ -64,15 +72,7 @@ pshy.perms.everyone["!vampire"] = false
 
 --- !cheese
 function pshy.ChatCommandCheese(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!cheese-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!cheese")
 	if not tfm.get.room.playerList[target].hasCheese then
 		tfm.exec.giveCheese(target)
 	else
@@ -87,15 +87,7 @@ pshy.perms.everyone["!cheese"] = true
 
 --- !freeze
 function pshy.ChatCommandFreeze(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!freeze-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!freeze")
 	tfm.exec.freezePlayer(target, true)
 end
 pshy.chat_commands["freeze"] = {func = pshy.ChatCommandFreeze, desc = "freeze yourself", argc_min = 0, argc_max = 1, arg_types = {"string"}}
@@ -106,15 +98,7 @@ pshy.perms.everyone["!freeze"] = true
 
 --- !kill
 function pshy.ChatCommandKill(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!kill-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!kill")
 	if not tfm.get.room.playerList[target].isDead then
 		tfm.exec.killPlayer(target)
 	else
@@ -129,15 +113,7 @@ pshy.perms.everyone["!kill"] = true
 
 --- !win
 function pshy.ChatCommandWin(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!win-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!win")
 	tfm.exec.playerVictory(target)
 end
 pshy.chat_commands["win"] = {func = pshy.ChatCommandWin, desc = "play the win animation", argc_min = 0, argc_max = 1, arg_types = {"string"}}
@@ -148,15 +124,7 @@ pshy.perms.everyone["!win"] = true
 
 --- !colorpicker
 function pshy.ChatCommandColorpicker(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!colorpicker-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!colorpicker")
 	ui.showColorPicker(49, target, 0, "Get a color code:")
 end 
 pshy.chat_commands["colorpicker"] = {func = pshy.ChatCommandColorpicker, desc = "show the colorpicker", argc_min = 0, argc_max = 1, arg_types = {"string"}}
@@ -167,15 +135,7 @@ pshy.perms.everyone["!colorpicker"] = true
 
 --- !fly
 function pshy.ChatCommandFly(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!fly-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!fly")
 	if not pshy.fun_commands_flyers[target] then
 		pshy.fun_commands_flyers[target] = true
 		tfm.exec.bindKeyboard(target, 1, true, true)
@@ -194,15 +154,7 @@ pshy.perms.everyone["!fly"] = true
 
 --- !speed
 function pshy.ChatCommandSpeed(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!fly-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!speed")
 	if not pshy.fun_commands_speedies[target] then
 		pshy.fun_commands_speedies[target] = true
 		tfm.exec.bindKeyboard(target, 0, true, true)
@@ -231,15 +183,7 @@ pshy.perms.everyone["!action"] = true
 
 --- !balloon
 function pshy.ChatCommandBalloon(user, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!balloon-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!balloon")
 	tfm.exec.attachBalloon(target, true, math.random(1, 4), true)
 end 
 pshy.chat_commands["balloon"] = {func = pshy.ChatCommandBalloon, desc = "attach a balloon to yourself", argc_min = 0, argc_max = 1, arg_types = {"string"}}
@@ -250,17 +194,9 @@ pshy.perms.everyone["!balloon"] = false
 
 --- !size <n>
 function pshy.ChatCommandSize(user, size, target)
-	assert(size > 0.2, "minimum size is 0.2")
-	assert(size < 5, "maximum size is 5")
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!size-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	assert(size >= 0.2, "minimum size is 0.2")
+	assert(size <= 5, "maximum size is 5")
+	target = pshy.fun_commands_GetTarget(user, target, "!size")
 	tfm.exec.changePlayerSize(target, size)
 end 
 pshy.chat_commands["size"] = {func = pshy.ChatCommandSize, desc = "change your size", argc_min = 1, argc_max = 2, arg_types = {"number", "string"}}
@@ -271,15 +207,7 @@ pshy.perms.everyone["!size"] = true
 
 --- !namecolor
 function pshy.ChatCommandNamecolor(user, color, target)
-	if not target then
-		target = user
-	elseif not pshy.HavePerm(user, "!namecolor-others") then
-		error("You are not allowed to use this command on others :c")
-		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
+	target = pshy.fun_commands_GetTarget(user, target, "!namecolor")
 	tfm.exec.setNameColor(target, color)
 end 
 pshy.chat_commands["namecolor"] = {func = pshy.ChatCommandNamecolor, desc = "change your name's color", argc_min = 1, argc_max = 2, arg_types = {nil, "string"}}
@@ -299,19 +227,13 @@ pshy.help_pages["pshy_fun_commands"].commands["gravity"] = pshy.chat_commands["g
 
 --- !link
 function pshy.ChatCommandLink(user, wish, target)
-	if not target then
-		target = user
-		pshy.fun_commands_link_wishes[user] = wish
-	elseif not pshy.HavePerm(user, "!link-others") then
-		error("You are not allowed to use this command on others :c")
+	target = pshy.fun_commands_GetTarget(user, target, "!link")
+	if wish == "off" then
+		tfm.exec.linkMice(target, target, false)
 		return
-	elseif not tfm.get.room.playerList[target] then
-		error("This player is not in the room.")
-		return
-	end
-	if not wish then
-		-- delete link
-		tfm.exec.linkMice(target, target, true)
+	else
+		wish = pshy.FindPlayerNameOrError(wish)
+		pshy.fun_commands_link_wishes[target] = wish
 	end
 	if wish == target then
 		tfm.exec.linkMice(target, wish, false)
@@ -319,7 +241,7 @@ function pshy.ChatCommandLink(user, wish, target)
 		tfm.exec.linkMice(target, wish, true)
 	end
 end 
-pshy.chat_commands["link"] = {func = pshy.ChatCommandLink, desc = "attach yourself to another player (they need to use the command too / yourself to stop)", argc_min = 1, argc_max = 2, arg_types = {"string", "string"}}
+pshy.chat_commands["link"] = {func = pshy.ChatCommandLink, desc = "attach yourself to another player (they need to use the command too / 'off' to stop)", argc_min = 1, argc_max = 2, arg_types = {"string", "string"}}
 pshy.help_pages["pshy_fun_commands"].commands["link"] = pshy.chat_commands["link"]
 pshy.perms.everyone["!link"] = true
 
