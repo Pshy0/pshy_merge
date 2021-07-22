05e1e925ae5f268f7509e94d4c05684a22d8e093
redacted <redacted>
2021-07-22 17:01:40 +0200

    added pshy_bindkey

---
+++ modules/pshy/pshy_bindkey.lua
@@ -0,0 +1,63 @@
+--- pshy_bindkey.lua
+--
+-- Bind your keys to a command.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+-- @namespace pshy
+-- @require pshy_keycodes.lua
+-- @require pshy_commands.lua
+-- @require pshy_help.lua
+
+
+
+--- Module Help Page:
+pshy.help_pages["pshy_bindkey"] = {back = "pshy", title = "Key Binds", text = "Bind a command to a key (use $d and $d for x and y)\n", commands = {}}
+pshy.help_pages["pshy"].subpages["pshy_bindkey"] = pshy.help_pages["pshy_bindkey"]
+
+
+
+--- Internal use:
+pshy.bindkey_players_binds = {}			-- players binds
+
+
+
+--- TFM event eventKeyboard.
+function eventKeyboard(player_name, key_code, down, x, y)
+	if pshy.bindkey_players_binds[player_name] then
+		local binds = pshy.bindkey_players_binds[player_name]
+		if binds[key_code] then
+			local cmd = string.format(binds[key_code], x, y) -- only in Lua!
+			eventChatCommand(cmd)
+			return false
+		end
+	end
+end
+
+
+
+--- !bindkey <key> [command]
+function pshy.bindkey_ChatCommandBindkey(user, keyname, command)
+	keycode = tonumber(keyname)
+	if not keycode then
+		keycode = pshy.keycodes[keyname]
+	end
+	if not keycode then
+		return false, "unknown key, use the KEY_NAME ('A', 'SLASH', 'NUMPAD_ADD', ...)"
+	end
+	if string.sub(command, 1, 1) == "!" then
+		command = string.sub(command, 2, #command)
+	end
+	pshy.bindkey_players_binds[user] = pshy.bindkey_players_binds[user] or {}
+	local binds = pshy.bindkey_players_binds[user]
+	if command == nil then
+		binds[keycode] = nil
+		tfm.exec.chatMessage("Key bind disabled.", user)
+	else
+		binds[keycode] = command
+		tfm.exec.chatMessage("Key bound to `" .. command .. "`.", user)
+		tfm.exec.bindkeyboard(user, keycode, true, true)
+	end
+end
+pshy.chat_commands["bindkey"] = {func = pshy.bindkey_ChatCommandBindkey, desc = "bind a command to a key, use $d and $d for coordinates", argc_min = 0, argc_max = 1, arg_types = {"string", "string"}, arg_names = {"KEYNAME", "command"}}
+pshy.help_pages["pshy_bindmouse"].commands["bindkey"] = pshy.chat_commands["bindkey"]
+pshy.perms.everyone["!bindmouse"] = false
