6bc4d60cf410dd99908093b1dcf785bda1f71825
redacted <redacted>
2021-08-02 18:34:10 +0200

    added search function (testing only)

--- lua/pshy/db/pshy_imagedb.lua
+++ lua/pshy/db/pshy_imagedb.lua
@@ -47,46 +47,64 @@ pshy.imagedb_images["17ae4716720.png"] = {pacman = true, w = 25, desc = "cherry"
 pshy.imagedb_images["17ae4717e93.png"] = {pacman = true, w = 25, desc = "biscuit"}
 pshy.imagedb_images["17ae4719605.png"] = {pacman = true, w = 25, desc = "carrot"}
 -- emoticons
-pshy.imagedb_images["16f56cbc4d7.png"] = {emoticon = true, author = "", desc = "nausea"}
-pshy.imagedb_images["17088661168.png"] = {emoticon = true, author = "", desc = "cry"}
-pshy.imagedb_images["16f5d8c7401.png"] = {emoticon = true, author = "", desc = "rogue"}
+pshy.imagedb_images["16f56cbc4d7.png"] = {emoticon = true, author = "", w = 29, h = 26, desc = "nausea"}
+pshy.imagedb_images["17088661168.png"] = {emoticon = true, author = "", w = 29, h = 26, desc = "cry"}
+pshy.imagedb_images["16f5d8c7401.png"] = {emoticon = true, author = "", w = 29, h = 26, desc = "rogue"}
 pshy.imagedb_images["16f56ce925e.png"] = {emoticon = true, author = "", desc = "happy cry"}
 pshy.imagedb_images["16f56cdf28f.png"] = {emoticon = true, author = "", desc = "wonder"}
 pshy.imagedb_images["16f56d09dc2.png"] = {emoticon = true, author = "", desc = "happy cry 2"}
-pshy.imagedb_images["178ea94a353.png"] = {emoticon = true, author = "", desc = "vanlike novoice"}
+pshy.imagedb_images["178ea94a353.png"] = {emoticon = true, author = "", w = 35, h = 30, desc = "vanlike novoice"}
 pshy.imagedb_images["178ea9d3ff4.png"] = {emoticon = true, author = "", desc = "vanlike vomit"}
 pshy.imagedb_images["178ea9d5bc3.png"] = {emoticon = true, author = "", desc = "vanlike big eyes"}
 pshy.imagedb_images["178ea9d7876.png"] = {emoticon = true, author = "", desc = "vanlike pinklove"}
 pshy.imagedb_images["178ea9d947c.png"] = {emoticon = true, author = "", desc = "vanlike eyelove"}
-pshy.imagedb_images["178eac181f1.png"] = {emoticon = true, author = "rchl#0000", desc = "drawing zzz"}
+pshy.imagedb_images["178eac181f1.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 28, desc = "drawing zzz"}
 pshy.imagedb_images["178ebdf194a.png"] = {emoticon = true, author = "rchl#0000", desc = "glasses1"}
 pshy.imagedb_images["178ebdf317a.png"] = {emoticon = true, author = "rchl#0000", desc = "glasses2"}
-pshy.imagedb_images["178ebdf0153.png"] = {emoticon = true, author = "rchl#0000", desc = "clown"}
-pshy.imagedb_images["178ebdf495d.png"] = {emoticon = true, author = "rchl#0000", desc = "sad"}
-pshy.imagedb_images["178ebdee617.png"] = {emoticon = true, author = "rchl#0000", desc = "vomit"}
-pshy.imagedb_images["17aa125e853.png"] = {emoticon = true, author = "rchl#0000", desc = "sad2"}
+pshy.imagedb_images["178ebdf0153.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 31, desc = "clown"}
+pshy.imagedb_images["178ebdee617.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 31, desc = "vomit"}
+pshy.imagedb_images["178ebdf495d.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 31, desc = "sad"}
+pshy.imagedb_images["17aa125e853.png"] = {emoticon = true, author = "rchl#0000", w = 48, h = 48, desc = "sad2"}
 pshy.imagedb_images["17aa1265ea4.png"] = {emoticon = true, author = "feverchild#0000", desc = "ZZZ"} -- https://discord.com/channels/246815328103825409/522398576706322454/834007372640419851
 pshy.imagedb_images["17aa1264731.png"] = {emoticon = true, author = "feverchild#0000", desc = "no voice"}
-pshy.imagedb_images["17aa1bcf1d4.png"] = {emoticon = true, author = "Nnaaaz#0000", desc = "pro"}
-pshy.imagedb_images["17aa1bd3a05.png"] = {emoticon = true, author = "Nnaaaz#0000", desc = "noob"}
+pshy.imagedb_images["17aa1bcf1d4.png"] = {emoticon = true, author = "Nnaaaz#0000", w = 60, h = 60, desc = "pro"}
+pshy.imagedb_images["17aa1bd3a05.png"] = {emoticon = true, author = "Nnaaaz#0000", w = 60, h = 49, desc = "noob"}
 pshy.imagedb_images["17aa1bd0944.png"] = {emoticon = true, author = "Nnaaaz#0000", desc = "pro2"}
 pshy.imagedb_images["17aa1bd20b5.png"] = {emoticon = true, author = "Nnaaaz#0000", desc = "noob2"}
 -- memes
 pshy.imagedb_images["15565dbc655.png"] = {meme = true, desc = "WTF cat"} -- https://atelier801.com/topic?f=6&t=827044&p=1#m14
-pshy.imagedb_images["15568238225.png"] = {meme = true, desc = "FUUU"}
+pshy.imagedb_images["15568238225.png"] = {meme = true, w = 40, h = 40, desc = "FUUU"}
 pshy.imagedb_images["155682434d5.png"] = {meme = true, desc = "me gusta"}
-pshy.imagedb_images["1556824ac1a.png"] = {meme = true, desc = "trollface"}
+pshy.imagedb_images["1556824ac1a.png"] = {meme = true, w = 40, h = 40, desc = "trollface"}
 -- TFM
-pshy.imagedb_images["155593003fc.png"] = {TFM = true, desc = "cheese left"}
-pshy.imagedb_images["155592fd7d0.png"] = {TFM = true, desc = "cheese right"}
+pshy.imagedb_images["155593003fc.png"] = {TFM = true, w = 48, h = 29, desc = "cheese left"}
+pshy.imagedb_images["155592fd7d0.png"] = {TFM = true, w = 48, h = 29, desc = "cheese right"}
 -- Mario
-pshy.imagedb_images["156d7dafb2d.png"] = {mario = true, desc = "mario"}
-pshy.imagedb_images["17aa6f22c53.png"] = {mario = true, desc = "mario coin"}
+pshy.imagedb_images["156d7dafb2d.png"] = {mario = true, desc = "mario (undersized)"} -- @TODO: replace whith a properly sized image
+pshy.imagedb_images["17aa6f22c53.png"] = {mario = true, w = 27, h = 38, desc = "coin"}
 -- Pokemon
 
 
 
+--- Search for an image.
+-- @private
+-- This function is currently for testing only.
+-- @param desc Text to find in the image's description.
+-- @param category to search in.
+-- @return the first image matching the search.
+function pshy.imagedb_Search(desc, category)
+	for image_name, image in pairs(pshy.imagedb_images) do
+		if image.desc and string.find(image.desc, desc) and (not category or image[category]) then
+			return image_name
+		end
+	end
+	return nil
+end
+
+
+
 --- Draw an image (wrapper to tfm.exec.addImage).
+-- @public
 -- @param image_name The image code (called imageId in te original function).
 -- @param target On what game element to attach the image to.
 -- @param center_x Center coordinates for the image.
