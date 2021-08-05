18cd92ba07f8167c92e8bb9f8c70dc620101c8eb
redacted <redacted>
2021-08-05 03:59:51 +0200

    updated library

--- lua/pshy/db/pshy_imagedb.lua
+++ lua/pshy/db/pshy_imagedb.lua
@@ -30,7 +30,93 @@ pshy.imagedb_max_search_results = 20		-- maximum search displayed results
 --	- h: The pixel height of the picture (default to `w`).
 pshy.imagedb_images = {}
 -- model
-pshy.imagedb_images["00000000000.png"] = {w = nil, h = nil, author = "", desc = ""}
+pshy.imagedb_images["00000000000.png"] = {w = nil, h = nil, desc = ""}
+-- pixels (source: Peanut_butter https://atelier801.com/topic?f=6&t=827044&p=1#m12)
+pshy.imagedb_images["165965055b2.png"] = {author = "Dea_bu#0000", w = 25, h = 34, desc = "pixel 1"}
+pshy.imagedb_images["1659658dc8f.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 2"}
+pshy.imagedb_images["165966b6346.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 3"}
+pshy.imagedb_images["165966cc2db.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 4"}
+pshy.imagedb_images["165966d9a68.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 5"}
+pshy.imagedb_images["165966f86f6.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 6"}
+pshy.imagedb_images["16596700568.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 7"}
+pshy.imagedb_images["165967088be.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 8"}
+pshy.imagedb_images["1659671b6fb.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 9"}
+pshy.imagedb_images["16596720dd2.png"] = {author = "Dea_bu#0000", w = 25, h = 34, desc = "pixel 10"}
+pshy.imagedb_images["1659672d821.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 11"}
+pshy.imagedb_images["16596736237.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 12"}
+pshy.imagedb_images["1659673b8d5.png"] = {author = "Dea_bu#0000", w = 25, h = 30, desc = "pixel 13"}
+pshy.imagedb_images["16596740a8f.png"] = {author = "Dea_bu#0000", w = 25, h = 34, desc = "pixel 14"}
+pshy.imagedb_images["16596746e71.png"] = {author = "Dea_bu#0000", w = 25, h = 34, desc = "pixel 15"}
+-- flags (source: Bolodefchoco https://atelier801.com/topic?f=6&t=877911#m1)
+pshy.imagedb_images["1651b327097.png"] = {w = 16, h = 11, desc = "xx flag"}
+pshy.imagedb_images["1651b32290a.png"] = {w = 16, h = 11, desc = "ar flag"}
+pshy.imagedb_images["1651b300203.png"] = {w = 16, h = 11, desc = "bg flag"}
+pshy.imagedb_images["1651b3019c0.png"] = {w = 16, h = 11, desc = "br flag"}
+pshy.imagedb_images["1651b3031bf.png"] = {w = 16, h = 11, desc = "cn flag"}
+pshy.imagedb_images["1651b304972.png"] = {w = 16, h = 11, desc = "cz flag"}
+pshy.imagedb_images["1651b306152.png"] = {w = 16, h = 11, desc = "de flag"}
+pshy.imagedb_images["1651b307973.png"] = {w = 16, h = 11, desc = "ee flag"}
+pshy.imagedb_images["1651b309222.png"] = {w = 16, h = 11, desc = "es flag"}
+pshy.imagedb_images["1651b30aa94.png"] = {w = 16, h = 11, desc = "fi flag"}
+pshy.imagedb_images["1651b30c284.png"] = {w = 16, h = 11, desc = "fr flag"}
+pshy.imagedb_images["1651b30da90.png"] = {w = 16, h = 11, desc = "gb flag"}
+pshy.imagedb_images["1651b30f25d.png"] = {w = 16, h = 11, desc = "hr flag"}
+pshy.imagedb_images["1651b310a3b.png"] = {w = 16, h = 11, desc = "hu flag"}
+pshy.imagedb_images["1651b3121ec.png"] = {w = 16, h = 11, desc = "id flag"}
+pshy.imagedb_images["1651b3139ed.png"] = {w = 16, h = 11, desc = "il flag"}
+pshy.imagedb_images["1651b3151ac.png"] = {w = 16, h = 11, desc = "it flag"}
+pshy.imagedb_images["1651b31696a.png"] = {w = 16, h = 11, desc = "jp flag"}
+pshy.imagedb_images["1651b31811c.png"] = {w = 16, h = 11, desc = "lt flag"}
+pshy.imagedb_images["1651b319906.png"] = {w = 16, h = 11, desc = "lv flag"}
+pshy.imagedb_images["1651b31b0dc.png"] = {w = 16, h = 11, desc = "nl flag"}
+pshy.imagedb_images["1651b31c891.png"] = {w = 16, h = 11, desc = "ph flag"}
+pshy.imagedb_images["1651b31e0cf.png"] = {w = 16, h = 11, desc = "pl flag"}
+pshy.imagedb_images["1651b31f950.png"] = {w = 16, h = 11, desc = "ro flag"}
+pshy.imagedb_images["1651b321113.png"] = {w = 16, h = 11, desc = "ru flag"}
+pshy.imagedb_images["1651b3240e8.png"] = {w = 16, h = 11, desc = "tr flag"}
+pshy.imagedb_images["1651b3258b3.png"] = {w = 16, h = 11, desc = "vk flag"}
+-- Memes (source: Zubki https://atelier801.com/topic?f=6&t=827044&p=1#m1)
+--@TODO  (40;50)
+-- Misc (source: Shamousey https://atelier801.com/topic?f=6&t=827044&p=1#m5)
+--@TODO
+-- Jerry (source: Noooooooorr https://atelier801.com/topic?f=6&t=827044&p=1#m13)
+pshy.imagedb_images["174d14019e2.png"] = {w = 86, h = 90, desc = "jerry 1"}
+pshy.imagedb_images["174d12f1634.png"] = {w = 61, h = 80, desc = "jerry 2"}
+pshy.imagedb_images["1717581457e.png"] = {w = 70, h = 100, desc = "jerry 3"}
+pshy.imagedb_images["171524ab085.png"] = {w = 67, h = 60, desc = "jerry 4"}
+pshy.imagedb_images["1740c7d4de6.png"] = {w = 80, h = 72, desc = "jerry 5"}
+pshy.imagedb_images["1718e698ac9.png"] = {w = 85, h = 110, desc = "jerry 6"}
+pshy.imagedb_images["17526faf702.png"] = {w = 80, h = 50, desc = "jerry 7"}
+pshy.imagedb_images["17526fc5a1c.png"] = {w = 70, h = 73, desc = "jerry 8"}
+pshy.imagedb_images["1792c9c8635.png"] = {w = 259, h = 290, desc = "hungry nibbbles"}
+-- Among us (source: Noooooooorr https://atelier801.com/topic?f=6&t=827044&p=1#m13)
+pshy.imagedb_images["174d9e0072e.png"] = {w = 37, h = 50, desc = "among us red"}
+pshy.imagedb_images["174d9e01e9e.png"] = {w = 37, h = 50, desc = "among us cyan"}
+pshy.imagedb_images["174d9e03612.png"] = {w = 37, h = 50, desc = "among us blue"}
+pshy.imagedb_images["174d9e0c2be.png"] = {w = 37, h = 50, desc = "among us purple"}
+pshy.imagedb_images["174d9e04d84.png"] = {w = 37, h = 50, desc = "among us green"}
+pshy.imagedb_images["174d9e064f6.png"] = {w = 37, h = 50, desc = "among us pink"}
+pshy.imagedb_images["174d9e07c67.png"] = {w = 37, h = 50, desc = "among us yellow"}
+pshy.imagedb_images["174d9e093d9.png"] = {w = 37, h = 50, desc = "among us black"}
+pshy.imagedb_images["174d9e0ab49.png"] = {w = 37, h = 50, desc = "among us white"}
+pshy.imagedb_images["174da01d1ae.png"] = {w = 24, h = 30, desc = "among us mini white"}
+-- misc (source: Noooooooorr https://atelier801.com/topic?f=6&t=827044&p=1#m14)
+pshy.imagedb_images["1789e6b9058.png"] = {w = 245, h = 264, desc = "skeleton", TFM = true}
+pshy.imagedb_images["178cbf1ff84.png"] = {w = 280, h = 290, desc = "meli mouse", TFM = true}
+pshy.imagedb_images["1792c9cd64e.png"] = {w = 290, h = 390, desc = "skeleton cat", TFM = true}
+pshy.imagedb_images["1789d45e0a4.png"] = {w = 234, h = 280, desc = "explorer dora", TFM = true}
+-- misc (source: Wercade https://atelier801.com/topic?f=6&t=827044&p=1#m10)
+pshy.imagedb_images["1557c364a52.png"] = {w = 150, h = 100, desc = "mouse"} -- @TODO: resize
+pshy.imagedb_images["155c49d0331.png"] = {w = 60, h = 33, desc = "horse"}
+pshy.imagedb_images["155c4a31e48.png"] = {w = 50, h = 49,  desc = "poop", oriented = false}
+pshy.imagedb_images["155ca47179a.png"] = {w = 74, h = 50, desc = "computer mouse"}
+pshy.imagedb_images["155c9e6aad4.png"] = {w = 60, h = 50, desc = "toilet paper"}
+pshy.imagedb_images["155c5133917.png"] = {w = 70, h = 45, desc = "waddles pig"}
+pshy.imagedb_images["155c4cdd0e3.png"] = {w = 50, h = 51, desc = "cock"}
+pshy.imagedb_images["155c4976244.png"] = {w = 60, h = 50, desc = "sponge bob"}
+pshy.imagedb_images["155c9fab3f1.png"] = {w = 72, h = 60, desc = "mouse on broom", TFM = true}
+-- gravity falls (source: Breathin https://atelier801.com/topic?f=6&t=827044&p=1#m15)
+pshy.imagedb_images["17a52468a34.png"] = {w = 30, h = 50, desc = "waddles pig sitting"}
 -- pacman (Made by Nnaaaz#0000)
 pshy.imagedb_images["17ad578a939.png"] = {pacman = true, w = 45, author = "Nnaaaz#0000", desc = "open pacman"}
 pshy.imagedb_images["17ad578c0aa.png"] = {pacman = true, w = 45, author = "Nnaaaz#0000", desc = "closed pacman"}
@@ -92,10 +178,80 @@ pshy.imagedb_images["1556824ac1a.png"] = {meme = true, w = 40, h = 40, desc = "t
 -- TFM
 pshy.imagedb_images["155593003fc.png"] = {TFM = true, w = 48, h = 29, desc = "cheese left"}
 pshy.imagedb_images["155592fd7d0.png"] = {TFM = true, w = 48, h = 29, desc = "cheese right"}
+pshy.imagedb_images["153d331c6b9.png"] = {TFM = true, desc = "normal mouse"}
+-- TFM (source: Laagaadoo https://atelier801.com/topic?f=6&t=877911#m3)
+--1569ed22fca.png - Estante de livros
+--1569edb5d05.png - Estante de livros (invertida)
+--1569ec80946.png - Lareira
+--15699c75f35.png - Lareira (invertida)
+--1569e9e54f4.png - Caixão
+--15699c67278.png - Caixão (invertido)
+--1569e7e4495.png - Cemiterio
+--156999e1f40.png - Cemiterio (invertido)
+--156999ebf03.png - Árvore de natal
+--1569e7d3bac.png - Arvore de natal (invertida)
+--1569e7ca20e.png - Arvore com neve
+--156999e6b7e.png - Árvore com neve (invertida)
+--155a7b9a815.png - Árvore
+--1569e788f68.png - Árvore (invertida)
+--155a7c4e15a.png - Flor vermelha
+--155a7c50a6b.png - Flor azul
+--155a7c834a4.png - Janela
+--1569e9bfb87.png - Janela (invertida)
+--155a7ca38b7.png - Sofá
+--156999f093a.png - Palmeira
+--1569e7706c4.png - Palmeira (invertido)
+--15699b2da1f.png - Estante de halloween
+--1569e77e3a5.png - Estante de halloween (invertido)
+--1569e79c9e3.png - Árvore do outono
+--15699b344da.png - Árvore do outono (invertida)
+--1569e773235.png - Abobora gigante
+--15699c5e038.png - Piano
+--15699c3eedd.png - Barril
+--15699b15524.png - Guada roupa
+--1569e7ae2e0.png - Guarda roupa (invertido)
+--1569edb8321.png - Baú
+--1569ed263b4.png - Baú (invertido)
+--1569edbaea9.png - Postêr
+--1569ed28f41.png - Postêr (invertido)
+--1569ed2cb80.png - Boneco de neve
+--1569edbe194.png - Boneco de neve (invertido)
+-- backgrounds (source: Travonrodfer https://atelier801.com/topic?f=6&t=877911#m6)
+--14e555a4c1b.jpg - Mapa Independence Day
+--14e520635b4.png - Estatua da liberdade(Mapa Independence Day)
+--14e78118c13.jpg - Mapa Bastille Day
+--14e7811b53a.png - Folha das arvores(Mapa Bastille Day)
+--149c04b50ac.jpg - Mapa do ceifador
+--149c04bc447.png - Mapa do ceifador(partes em primeiro plano)
+--14abae230c8.jpg - Mapa Rua Nuremberg
+--14aa6e36f3e.png - Mapa Rua Nuremberg(partes em primeiro plano)
+--14a88571f89.jpg - Mapa Fabrica de brinquedos
+--14a8d41a838.jpg - Mapa dia das crianças
+--14a8d430dfa.png - Mapa dia das crianças(partes em primeiro plano)
+--15150c10e92.png - Mapa de ano novo
+-- TFM Particles (source: Tempo https://atelier801.com/topic?f=6&t=877911#m7)
+--1674801ea08.png ~> Raiva
+--16748020179.png ~> Palmas
+--167480218ea.png ~> Confete
+--1674802305b.png ~> Dança
+--167480247cc.png ~> Facepalm
+--16748025f3d.png ~> High five
+--167480276af.png ~> Abraçar
+--16748028e21.png ~> Pedir Beijo
+--1674802a592.png ~> Beijar
+--1674802bd07.png ~> Risada
+--1674802d478.png ~> Pedra papel tesoura
+--1674802ebea.png ~> Sentar
+--1674803035b.png ~> Dormir
+--16748031acc.png ~> Chorar
 -- Mario
 pshy.imagedb_images["156d7dafb2d.png"] = {mario = true, desc = "mario (undersized)"} -- @TODO: replace whith a properly sized image
 pshy.imagedb_images["17aa6f22c53.png"] = {mario = true, w = 27, h = 38, desc = "coin"}
--- Pokemon
+-- Pokemon (source: Shamousey https://atelier801.com/topic?f=6&t=827044&p=1#m6)
+--@TODO
+
+
+--- Tell
 
 
 
