a8093054e7f616d2eba62d639fb32b6b9e24150c
redacted <redacted>
2021-08-08 01:06:06 +0200

    updated pshyfun

--- lua/packs/modulepack_pshyfun.lua
+++ lua/packs/modulepack_pshyfun.lua
@@ -5,19 +5,17 @@
 -- @author pshy
 --
 -- @hardmerge
--- @require pshy_merge.lua
--- @require pshy_commands.lua
 -- @require pshy_lua_commands.lua
 -- @require pshy_fun_commands.lua
+-- @require pshy_tfm_commands.lua
 -- @require pshy_speedfly.lua
 -- @require pshy_fcplatform.lua
 -- @require pshy_nicks.lua
 -- @require pshy_checkpoints.lua
 -- @require pshy_motd.lua
--- @require pshy_weather.lua
 -- @require pshy_basic_weathers.lua
--- @require pshy_rotations.lua
 -- @require pshy_emoticons.lua
+-- @require pshy_changeimage.lua
 -- @require pshy_requests.lua
 
 
@@ -27,6 +25,7 @@ pshy.perms_cheats_enabled = true
 pshy.rotations_auto_next_map = false
 pshy.help_pages[""].subpages["pshy_fun_commands"] = pshy.help_pages["pshy_fun_commands"]
 pshy.help_pages[""].subpages["pshy_emoticons"] = pshy.help_pages["pshy_emoticons"]
+pshy.help_pages[""].subpages["pshy_speedfly"] = pshy.help_pages["pshy_speedfly"]
 
 
 
@@ -37,7 +36,7 @@ tfm.exec.disableAutoTimeLeft(true)
 tfm.exec.disableAllShamanSkills(true)		-- avoid skills induced lags
 tfm.exec.disableDebugCommand(true)
 tfm.exec.disableMinimalistMode(false)
-tfm.exec.disablePhysicalConsumables(true)
+tfm.exec.disablePhysicalConsumables(false)
 tfm.exec.disableWatchCommand(false)
 system.disableChatCommandDisplay(nil, true)
 --tfm.exec.disablePrespawnPreview(false)
