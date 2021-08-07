6078f432fe7cde2503112976ce439884f42bb1fb
redacted <redacted>
2021-08-07 21:21:14 +0200

    added proportions keeping addImageMin

--- lua/pshy/db/pshy_imagedb.lua
+++ lua/pshy/db/pshy_imagedb.lua
@@ -347,3 +347,31 @@ function pshy.imagedb_AddImage(image_name, target, center_x, center_y, player_na
 	local anchor_x, anchor_y = 0.5, 0.5
 	return tfm.exec.addImage(image_name, target, x, y, player_name, sx, sy, angle, alpha, anchor_x, anchor_y)
 end
+
+
+
+--- Draw an image (wrapper to tfm.exec.addImage) but keep the image dimentions (making it fit at least the given area).
+-- @public
+-- @param image_name The image code (called imageId in te original function).
+-- @param target On what game element to attach the image to.
+-- @param center_x Center coordinates for the image.
+-- @param center_y Center coordinates for the image.
+-- @param player_name The player who will see the image, or nil for everyone.
+-- @param width Width of the image.
+-- @param height Height of the image.
+-- @param angle The image's rotation (in radians).
+-- @param height Opacity of the image.
+-- @return The image ID.
+function pshy.imagedb_AddImageMin(image_name, target, center_x, center_y, player_name, width, height, angle, alpha)
+	local image = pshy.imagedb_images[image_name] or pshy.imagedb_images["15568238225.png"]
+	target = target or "!0"
+	width = width or image.w
+	height = height or image.h or image.w
+	local x = center_x + ((width > 0) and 0 or math.abs(width))-- - width / 2
+	local y = center_y + ((height > 0) and 0 or math.abs(height))-- - height / 2
+	local sx = width / (image.w)
+	local sy = height / (image.h or image.w)
+	local sboth = math.max(sx, sy)
+	local anchor_x, anchor_y = 0.5, 0.5
+	return tfm.exec.addImage(image_name, target, x, y, player_name, sboth, sboth, angle, alpha, anchor_x, anchor_y)
+end
