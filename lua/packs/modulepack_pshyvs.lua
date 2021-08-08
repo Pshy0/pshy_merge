5651edc9022a8dbbcfabbe28b647d9a0c824e1b3
redacted <redacted>
2021-08-08 02:17:47 +0200

    updated pshyfun

--- lua/packs/modulepack_pshyvs.lua
+++ lua/packs/modulepack_pshyvs.lua
@@ -4,16 +4,18 @@
 --
 -- @author pshy
 -- @hardmerge
--- @require pshy_emoticons.lua
--- @require pshy_lua_commands.lua
--- @require pshy_tfm_commands.lua
--- @require pshy_fun_commands.lua
--- @require pshy_fcplatform.lua
+-- @require pshy_changeimage.lua
 -- @require pshy_basic_weathers.lua
+-- @require pshy_fcplatform.lua
+-- @require pshy_fun_commands.lua
+-- @require pshy_emoticons.lua
+-- @require pshy_lobby.lua
+-- @require pshy_lua_commands.lua
 -- @require pshy_motd.lua
 -- @require pshy_nicks.lua
+-- @require pshy_speedfly.lua
 -- @require pshy_teams.lua
--- @require pshy_lobby.lua
+-- @require pshy_tfm_commands.lua
 
 
 
@@ -24,4 +26,5 @@ tfm.exec.disableMinimalistMode(false)
 tfm.exec.disablePhysicalConsumables(true)
 system.disableChatCommandDisplay(nil, true)
 tfm.exec.disableAutoShaman(true)
+tfm.exec.disableAfkDeath(true)
 --tfm.exec.disablePrespawnPreview(false)
