c2b61b4bb40e1049120918bc59f74a69c340dfe5
redacted <redacted>
2021-08-08 02:50:25 +0200

    increased image size limit to 400

--- lua/pshy/features/pshy_changeimage.lua
+++ lua/pshy/features/pshy_changeimage.lua
@@ -162,8 +162,8 @@ function pshy.changeimage_ChatCommandChangeimage(user, image_name, target)
 	if not image.w then
 		return false, "This image cannot be used (unknown width)."
 	end
-	if image.w > 200 or (image.h and image.h > 200)  then
-		return false, "This image is too big (w/h > 200)."
+	if image.w > 400 or (image.h and image.h > 400)  then
+		return false, "This image is too big (w/h > 400)."
 	end
 	pshy.changeimage_ChangeImage(target, image_name)
 end
