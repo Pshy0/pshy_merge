5bd38d4e9bd4fcec274c9e33f03e764314b29693
redacted <redacted>
2021-09-30 19:46:51 +0200

    disabled xml features for now as it is slow

--- lua/pshy/pshy_essentials.lua
+++ lua/pshy/pshy_essentials.lua
@@ -11,5 +11,5 @@
 -- `pshy_utils.lua` is a selection of useful basic functions.
 -- @require pshy_utils.lua
 -- `pshy_xmlmap.lua` parses xml maps so you can browse them.
--- @require pshy_xmlmap.lua
+--- @require pshy_xmlmap.lua
 
