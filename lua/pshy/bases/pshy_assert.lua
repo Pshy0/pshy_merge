9cbea82de0a37ebc9794a824298ea544ac245681
redacted <redacted>
2021-09-30 18:02:49 +0200

    added pshy_assert

---
+++ lua/pshy/bases/pshy_assert.lua
@@ -0,0 +1,21 @@
+--- pshy_assert.lua
+--
+-- Cause lua assert to provide more informations.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+pshy = pshy or {}
+
+
+
+--- Custom assert function.
+function pshy.assert(condition, message)
+	if not condition then
+		local error_message = "\n<u><r>ASSERTION FAILED</r></u>"
+		if message then
+			error_message = error_message .. "\n<b><o>" .. message .. "</o></b>"
+		end
+		error_message = error_message .. "\n<i><j>" .. debug.traceback() .. "</j></i>"
+		error(error_message)
+	end
+end
+assert = pshy.assert
