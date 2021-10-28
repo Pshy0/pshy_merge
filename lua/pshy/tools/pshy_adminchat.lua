031a73c2c65217a2a452b7eb3d8478fac16280b8
redacted <redacted>
2021-10-28 18:12:27 +0200

    added a function to send a message in the admin chat

--- lua/pshy/tools/pshy_adminchat.lua
+++ lua/pshy/tools/pshy_adminchat.lua
@@ -1,8 +1,8 @@
 --- pshy_adminchat.lua
 --
--- Add an admin chat.
+-- Add a room admin chat.
 --
--- @author Pshy
+-- @author TFM:Pshy#3752 DC:Pshy#7998
 -- @require pshy_commands.lua
 -- @require pshy_help.lua
 -- @require pshy_perms.lua
@@ -15,13 +15,26 @@ pshy.help_pages["pshy"].subpages["pshy_adminchat"] = pshy.help_pages["pshy_admin
 
 
 
+--- Send a message to room admins.
+function pshy.adminchat_Message(origin, message)
+	if not message then
+		message = origin
+		origin = "SCRIPT"
+	end
+	for admin in pairs(pshy.admins) do
+		tfm.exec.chatMessage("<r>⚔ [" .. origin .. "] <o>" .. message, admin)
+	end
+end
+
+
+
 --- !adminchat
-function pshy.ChatCommandAdminchat(user, message)
+local function ChatCommandAdminchat(user, message)
 	for admin in pairs(pshy.admins) do
 		tfm.exec.chatMessage("<r>⚔ [" .. user .. "] <ch2>" .. message, admin)
 	end
 end
-pshy.chat_commands["adminchat"] = {func = pshy.ChatCommandAdminchat, desc = "send a message to room admins", argc_min = 1, argc_max = 1, arg_types = {"string"}, arg_names = {"room-admin-only message"}}
+pshy.chat_commands["adminchat"] = {func = ChatCommandAdminchat, desc = "send a message to room admins", argc_min = 1, argc_max = 1, arg_types = {"string"}, arg_names = {"room-admin-only message"}}
 pshy.help_pages["pshy_adminchat"].commands["adminchat"] = pshy.chat_commands["adminchat"]
 pshy.perms.admins["!adminchat"] = true
 pshy.commands_aliases["ac"] = "adminchat"
