052c3580a15294d1be3293aec174648ae0d2a90b
redacted <redacted>
2021-12-08 02:59:11 +0100

    improved output

--- lua/pshy/utils/pshy_timing.lua
+++ lua/pshy/utils/pshy_timing.lua
@@ -18,13 +18,15 @@ pshy.timing_measures = {}
 function pshy.timing_Start(name)
 	local measure = pshy.timing_measures[name]
 	if not measure then
-		pshy.timing_times[name] = {}
+		pshy.timing_measures[name] = {}
 		measure = pshy.timing_measures[name]
 		measure.name = name
 		measure.total_time = 0
 		measure.start_count = 0
+		measure.total_count = 0
 	end
 	measure.start_count = measure.start_count + 1
+	measure.total_count = measure.total_count + 1
 	measure.start_time = os.time()
 end
 
@@ -32,11 +34,12 @@ end
 
 --- Stop measuring a time.
 function pshy.timing_Stop(name)
+	local measure = pshy.timing_measures[name]
+	assert(measure, "this measure does not exist")
+	assert(measure.start_count > 0, "this measure was not started")
 	measure.start_count = measure.start_count - 1
-	assert(measure.start_count > 0)
 	if measure.start_count == 0 then
-		local measure = pshy.timing_measures[name]
-		local elapsed = os.time() - measure.start_time
+	local elapsed = os.time() - measure.start_time
 		measure.total_time = measure.total_time + elapsed
 		measure.start_time = nil
 	end
@@ -46,7 +49,21 @@ end
 
 --- Print all measures.
 function pshy.timing_PrintMeasures()
+	print(string.format("<vi>Times at %u:</vi>", os.time()))
 	for measure_name, measure in pairs(pshy.timing_measures) do
-		print(string.format("<j>%s: %f", measure_name, measure.total_time))
+		print(string.format("<j>%s: <o>%dms</o> / <o>%dcalls</o> == <r>%.4f ms/call</r>", measure_name, measure.total_time, measure.total_count, measure.total_time/ measure.total_count))
 	end
+	return true, "Printed times to logs."
+end
+
+
+
+---  Reset measures.
+function pshy.timing_ResetMeasures()
+	print(string.format("<o>Times at %u:</o>", os.time()))
+	for measure_name, measure in pairs(pshy.timing_measures) do
+		measure.total_time = 0
+		measure.total_count = 0
+	end
+	return true, "Total times have been reset."
 end
