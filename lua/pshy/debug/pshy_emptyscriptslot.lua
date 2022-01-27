7865628eea256ba7aecec07e3906a53d1f885f71
redacted <redacted>
2022-01-27 02:34:37 +0100

    added empty script slot to essentials

---
+++ lua/pshy/debug/pshy_emptyscriptslot.lua
@@ -0,0 +1,8 @@
+--- pshy_emptyscriptslot.lua
+--
+-- This script is just to be inserted at the end, so that you can insert a custom script easily.
+--
+-- @require_priority 90
+--
+-- \/ INSERT YOUR SCRIPT JUST BELOW THIS LINE \/
+-- /\ INSERT YOUR SCRIPT JUST OVER THIS LINE /\
--- lua/pshy/pshy_essentials.lua
+++ lua/pshy/pshy_essentials.lua
@@ -6,6 +6,8 @@
 --
 -- `pshy_assert.lua` increase the informations provided by your asserts.
 -- @require pshy_assert.lua
+-- `pshy_emptyscriptslot.lua` allow easy adding of a custom script.
+-- @require pshy_emptyscriptslot.lua
 -- `pshy_print.lua` help formatting prints.
 -- @require pshy_print.lua
 -- `pshy_tools.lua` is a selection of useful room admin tools.
