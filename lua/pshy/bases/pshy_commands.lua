daf89a78cd5f8907d869c88907dfd10bed6e465d
redacted <redacted>
2022-01-30 19:18:00 +0100

    made function local

--- lua/pshy/bases/pshy_commands.lua
+++ lua/pshy/bases/pshy_commands.lua
@@ -81,7 +81,7 @@ end
 
 --- Get the real command name
 -- @param alias_name Command name or alias without `!`.
-function ResolveAlias(alias_name)
+local function ResolveAlias(alias_name)
 	while not pshy.commands[alias_name] and pshy.commands_aliases[alias_name] do
 		alias_name = pshy.commands_aliases[alias_name]
 	end
