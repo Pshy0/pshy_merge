62d23fb796454205dc1d7542a6977dedeb09a405
redacted <redacted>
2022-02-11 23:56:02 +0100

    added a pshyvs variant using pshy_commentator

---
+++ lua/pshy/games/pshyvs_with_commentator.lua
@@ -0,0 +1,13 @@
+--- pshyvs.lua
+--
+-- This file builds the pshyvs modulepack.
+--
+-- @author pshy
+-- @require pshyvs.lua
+-- @require pshy_nofuncorp.lua
+-- @require pshy_commentator.lua
+--
+-- @hardmerge
+
+
+
