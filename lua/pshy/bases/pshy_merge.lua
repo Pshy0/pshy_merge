e72226cb14e4485c2f7b6bf44f7f1d9f8eb121a8
Pshy0 <Pshy0@users.noreply.github.com>
2022-03-08 16:27:17 +0100

    split some features into pshy_version

--- lua/pshy/bases/pshy_merge.lua
+++ lua/pshy/bases/pshy_merge.lua
@@ -6,6 +6,8 @@
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 --
+-- @require pshy_version.lua
+--
 -- @hardmerge
 pshy = pshy or {}
 
@@ -14,15 +16,6 @@ pshy = pshy or {}
 --- Help Page
 pshy.help_pages = pshy.help_pages or {}						-- touching the help_pages table
 pshy.help_pages["pshy_merge"] = {title = "Merging (Modules)", text = "This module merge other modules, and can enable or disable them at any moment.", commands = {}}
-
-
-
---- Module Settings:
-__PSHY_TFM_VERSION__ = "7.89"						-- The last tfm version this script was made for.
-__PSHY_TFM_API_VERSION__ = "0.28"					-- The last tfm api version this script was made for.
-pshy.merge_days_before_update_request_1	= 14		-- How many days old the script should be before suggesting an update. `nil` to disable.
-pshy.merge_days_before_update_request_2	= 30		-- How many days old the script should be before requesting an update. `nil` to disable.
-pshy.merge_days_before_update_request_3	= nil		-- How many days old the script should be before refusing to start. `nil` to disable.
 
 
 
@@ -331,58 +324,5 @@ pshy.help_pages["pshy_merge"].commands["disablemodule"] = pshy.commands["disable
 
 
 
---- Perform initial misc checks and actions.
-function pshy.merge_Init()
-	print("<v>Pshy version <ch>" .. tostring(__PSHY_VERSION__) .. "</ch></v>")
-	-- check release age
-	local release_days = __PSHY_TIME__ / 60 / 60 / 24
-	local current_days = os.time() / 1000 / 60 / 60 / 24
-	local days_old = current_days - release_days
-	if pshy.merge_days_before_update_request_3 and days_old > pshy.merge_days_before_update_request_3 then
-		print(string.format("<r>This build is <vi>%d days</vi> old. Please consider obtaining a newer version.</r>", days_old))
-		error(string.format("<r>This build is <vi>%d days</vi> old. Please consider obtaining a newer version.</r>", days_old))
-	elseif pshy.merge_days_before_update_request_2 and days_old > pshy.merge_days_before_update_request_2 then
-		print(string.format("<o>This build is <r>%d days</r> old. Please obtain a newer version as soon as possible.</o>", days_old))
-	elseif pshy.merge_days_before_update_request_1 and days_old > pshy.merge_days_before_update_request_1 then
-		print(string.format("<j>This build is <o>%d days</o> old. An update may be available.</j>", days_old))
-	else
-		print(string.format("<v>This build is <ch>%d days</ch> old.</v>", days_old))
-	end
-	if pshy.merge_days_before_update_request_3 and days_old > pshy.merge_days_before_update_request_3 / 2 then
-		print(string.format("<r>/!\\ This script will not start after being %d days old.</r>", pshy.merge_days_before_update_request_3))
-	end
-	-- check tfm api version
-	local expected_tfm_api_version_numbers = {}
-	for number_str in string.gmatch(__PSHY_TFM_API_VERSION__, "([^\.]+)") do
-		table.insert(expected_tfm_api_version_numbers, tonumber(number_str))
-	end
-	local current_tfm_api_version_numbers = {}
-	for number_str in string.gmatch(tfm.get.misc.apiVersion, "([^\.]+)") do
-		table.insert(current_tfm_api_version_numbers, tonumber(number_str))
-	end
-	if current_tfm_api_version_numbers[1] and expected_tfm_api_version_numbers[1] ~= current_tfm_api_version_numbers[1] then
-		print("<o>The TFM LUA API had a major update, an update of pshy's script may be available for this new version.</o>")
-	elseif current_tfm_api_version_numbers[2] and expected_tfm_api_version_numbers[2] ~= current_tfm_api_version_numbers[2] then
-		print("<j>The TFM LUA API had a minor update, an update of pshy's script may be available for this new version.</j>")
-	end
-	-- check tfm version
-	local expected_tfm_version_numbers = {}
-	for number_str in string.gmatch(__PSHY_TFM_VERSION__, "([^\.]+)") do
-		table.insert(expected_tfm_version_numbers, tonumber(number_str))
-	end
-	local current_tfm_version_numbers = {}
-	for number_str in string.gmatch(tfm.get.misc.transformiceVersion, "([^\.]+)") do
-		table.insert(current_tfm_version_numbers, tonumber(number_str))
-	end
-	if current_tfm_version_numbers[1] and expected_tfm_version_numbers[1] ~= current_tfm_version_numbers[1] then
-		print("<o>Transformice had a major update, an update of pshy's script may be available for this new version.</o>")
-	elseif current_tfm_version_numbers[2] and expected_tfm_version_numbers[2] ~= current_tfm_version_numbers[2] then
-		print("<j>Transformice had a minor update, an update of pshy's script may be available for this new version.</j>")
-	end
-end
-
-
-
 -- Create pshy_merge.lua module
-pshy.merge_Init()
 pshy.merge_CreateModule("pshy_merge.lua")
