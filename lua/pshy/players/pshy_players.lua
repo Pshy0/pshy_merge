98a5d09e08cd9f452e6a7c05ee7dc68dae7a2a6e
Pshy0 <Pshy0@users.noreply.github.com>
2022-03-09 20:52:13 +0100

    removed unused features

--- lua/pshy/players/pshy_players.lua
+++ lua/pshy/players/pshy_players.lua
@@ -11,9 +11,6 @@
 --	- `has_sentinel_tag`		
 --	- `has_mapcrew_tag`				
 --	- `has_previous_staff_tag`		
---	- `alive`					`true` if the player is alive.
---	- `won`						`true` if the player has entered the hole.
---	- `cheeses`					How many cheeses this player have.
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 --
@@ -45,9 +42,6 @@ local function TouchPlayer(player_name)
 	new_player.has_sentinel_tag = (string.sub(player_name, -5) == "#0015")
 	new_player.has_mapcrew_tag = (string.sub(player_name, -5) == "#0020")
 	new_player.has_previous_staff_tag = (string.sub(player_name, -5) == "#0095")
-	new_player.alive = false
-	new_player.won = false
-	new_player.cheeses = 0
 	pshy.players[player_name] = new_player
 end
 
@@ -63,53 +57,6 @@ function eventPlayerLeft(player_name)
     if pshy.delete_players_on_leave then
     	pshy.players[player_name] = nil
     end
-	local player = pshy.players[player_name]
-	player.alive = false
-	player.cheeses = 0
-end
-
-
-
-function eventNewGame()
-	for player_name in pairs(tfm.get.room.playerList) do
-		local player = pshy.players[player_name]
-		player.alive = true
-		player.won = false
-		player.cheeses = 0
-	end
-end
-
-
-
-function eventPlayerWon(player_name)
-	local player = pshy.players[player_name]
-	player.alive = false
-	player.won = true
-	player.cheeses = 0
-end
-
-
-
-function eventPlayerDied(player_name)
-	pshy.players[player_name].alive = false
-end
-
-
-
-function eventPlayerGetCheese(player_name)
-	local player = pshy.players[player_name]
-	player.cheeses = player.cheeses + 1
-end
-
-
-
-function eventPlayerRespawn(player_name)
-	local player = pshy.players[player_name]
-	player.alive = true
-	if player.won then
-		player.won = false
-		player.cheeses = 0
-	end
 end
 
 
