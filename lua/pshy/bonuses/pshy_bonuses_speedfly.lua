4c73d4934c8a2e7f3fe4b9f4a720af1952781d9c
Pshy0 <Pshy0@users.noreply.github.com>
2022-03-14 17:39:26 +0100

    moved speed and fly bonuses to a different file

---
+++ lua/pshy/bonuses/pshy_bonuses_speedfly.lua
@@ -0,0 +1,26 @@
+--- pshy_basic_bonuses.lua
+--
+-- Bonuses using the speedfly module.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+--
+-- @require pshy_speedfly.lua
+-- @require pshy_bonuses.lua
+-- @require pshy_imagedb_bonuses.lua
+-- @require pshy_merge.lua
+
+
+
+--- BonusFly.
+function pshy.bonuses_callback_BonusFly(player_name, bonus)
+	pshy.speedfly_Fly(player_name, 50)
+end
+pshy.bonuses_types["BonusFly"] = {image = "17bf4b7250e.png", func = pshy.bonuses_callback_BonusFly}
+
+
+
+--- BonusHighSpeed.
+function pshy.bonuses_callback_BonusHighSpeed(player_name, bonus)
+	pshy.speedfly_Speed(player_name, 200)
+end
+pshy.bonuses_types["BonusHighSpeed"] = {image = "17bf4b9af56.png", func = pshy.bonuses_callback_BonusHighSpeed}
