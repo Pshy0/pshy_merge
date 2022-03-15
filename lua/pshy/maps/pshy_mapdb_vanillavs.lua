b094c90ddd0067c29b5de66b3515df59c4ca634d
Pshy0 <Pshy0@users.noreply.github.com>
2022-03-15 18:44:13 +0100

    exported some bonus maps

--- lua/pshy/maps/pshy_mapdb_vanillavs.lua
+++ lua/pshy/maps/pshy_mapdb_vanillavs.lua
@@ -17,12 +17,12 @@ pshy.mapdb_maps_vanilla_racing = {2, 11, 12, 19, 22, 40, 44, 45, 55, 57, 62, 67,
 pshy.mapdb_maps_vanilla_racing_pshy = {7897852, 7897853, 7897854, 7897855, 7897856, 7897858, 7897860, 7897862, 7897863, 7897864}
 --TODO: test and add: 7897857, 7897859
 --TODO: remove maps similar to: 7897853, 7897857, 7897859, 7897860, 7897863
-pshy.mapdb_maps_vanilla_racing_pshy_bonus = {"luatroll_v0_8"} -- 7876829, 7876830, 7838009, 7823124, 7812697, 
+pshy.mapdb_maps_vanilla_racing_pshy_bonus = {7899005}
 pshy.mapdb_maps_vanilla_racing_mattseba = {7833266, 7833263}
 pshy.mapdb_maps_vanilla_racing_mattseba_private = {7833271} -- basic maps only
 pshy.mapdb_maps_vanilla_racing_camjho = {7833268, 7833270, 7833288, 7833293, 7833289, 7833279, 7833291, 7833292, 7833282, 7833260}
 pshy.mapdb_maps_vanilla_racing_camjho_private = {7838914, 7838910, 7839352, 7833281, 7833269, 7839046, 7839942, 7833259, 7831136, 7833290, 7840122, 7840635} -- not on discord / permission obtained
-pshy.mapdb_maps_vanilla_racing_sebaslife_private = {7848738, 7839806, 7844660, 7848738, 7840379, 7844645, 7844642, 7844661, 7848782, 7844664, 7844984, 7839461, 7840186, 7838967, 7844648, 7839493, 7840176, 7839507, 7840728, 7840207, 7848605, 7840564, 7839014, 7844856} -- permission obtained
+pshy.mapdb_maps_vanilla_racing_sebaslife_private = {7848738, 7839806, 7844660, 7840379, 7844645, 7844642, 7844661, 7848782, 7844664, 7844984, 7839461, 7840186, 7838967, 7844648, 7839493, 7840176, 7839507, 7840728, 7840207, 7848605, 7840564, 7839014, 7844856} -- permission obtained
 pshy.mapdb_maps_vanilla_racing_notheav_private = {7863949, 7863951, 7863958, 7863953, 7863947, 7863955, 7863961} -- permission obtained
 pshy.mapdb_maps_vanilla_racing_keticoh = {2111371}
 pshy.mapdb_maps_vanilla_racing_epoki = {763961, 6714567}
