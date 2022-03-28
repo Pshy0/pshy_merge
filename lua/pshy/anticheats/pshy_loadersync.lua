cc0df12f48274a1cb23d7ad323c399daf89c4782
Pshy0 <Pshy0@users.noreply.github.com>
2022-03-28 19:12:01 +0200

    fixed logic

--- lua/pshy/anticheats/pshy_loadersync.lua
+++ lua/pshy/anticheats/pshy_loadersync.lua
@@ -25,13 +25,19 @@ end
 
 function eventPlayerLeft(player_name)
 	if player_name == pshy.loader then
+		tfm.exec.setPlayerSync(player_name)
+		print_info("pshy_loadersync: Loader returned and set as sync!")
+	else
 		for player_name in pairs(pshy.admins) do
-			tfm.exec.setPlayerSync(player_name)
-			return
+			if tfm.get.room.playerList[player_name] then
+				tfm.exec.setPlayerSync(player_name)
+				print_info("pshy_loadersync: Loader left, setting %s as sync!", player_name)
+				return
+			end
 		end
-		print_warn("pshy_loadersync: Loader left and no room admin were available, using a random player!")
 		for player_name in pairs(tfm.get.room.playerList) do
 			tfm.exec.setPlayerSync(player_name)
+			print_warn("pshy_loadersync: Loader left, setting %s as sync!", player_name)
 			return
 		end
 	end
