cc1cd8a94353ac6600316049c1b8f06984f6c868
Pshy0 <Pshy0@users.noreply.github.com>
2022-04-10 17:02:06 +0200

    moved anticheat commons to public

---
+++ lua/pshy/anticheats/pshy_anticheats_common.lua
@@ -0,0 +1,34 @@
+--- pshy_anticheats_common.lua
+--
+-- Base for anticheats module.
+-- This mainly provide the common help page.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+-- @require pshy_adminchat.lua
+-- @require pshy_ban.lua
+-- @require pshy_help.lua
+--
+-- @require_priority ANTICHEAT
+pshy = pshy or {}
+
+
+
+--- Module Help Page.
+pshy.help_pages["pshy_anticheats"] = {text = "Contains several anticheats scripts. See individual module's help.", commands = {}, subpages = {}}
+pshy.help_pages["pshy_anticheats"].restricted = true
+pshy.help_pages["pshy_anticheats"].back = "pshy"
+pshy.help_pages["pshy_anticheats"].title = "Anticheats"
+pshy.help_pages["pshy"].subpages["pshy_anticheats"] = pshy.help_pages["pshy_anticheats"]
+
+
+
+--- Ban Page:
+pshy.help_pages["pshy_anticheats"].subpages["pshy_ban"] = pshy.help_pages["pshy_ban"]
+
+
+
+function eventInit()
+	if pshy.perms_cheats_enabled then
+		pshy.adminchat_Message("Anticheat", "<b>YOU HAVE ENABLED AN ANTICHEAT IN A SCRIPT THAT IS ADDING CHEATS!!!</b>")
+	end
+end
