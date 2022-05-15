02ccd7f29143abd7bf9b73719558b9c416690420
Pshy0 <Pshy0@users.noreply.github.com>
2022-05-16 01:01:08 +0200

    handling a weird case

--- lua/pshy/bases/pshy_version.lua
+++ lua/pshy/bases/pshy_version.lua
@@ -1,8 +1,6 @@
 --- pshy_version.py
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
---
--- @hardmerge
 pshy = pshy or {}
 
 
@@ -19,6 +17,47 @@ __PSHY_TFM_API_VERSION__ = "0.28"					-- The last tfm api version this script wa
 pshy.version_days_before_update_suggested = 14		-- How old the script should be before suggesting an update (`nil` to disable).
 pshy.version_days_before_update_advised = 30		-- How old the script should be before requesting an update (`nil` to disable).
 pshy.version_days_before_update_required = nil		-- How old the script should be before refusing to start (`nil` to disable).
+
+
+
+--- Get a table of version numbers from a string representing that version.
+-- @param str_v String representing a version such as "8.1".
+local function StringToVersion(str_v)
+	local version_numbers = {}
+	for number_str in string.gmatch(str_v, "([^\.]+)") do
+		table.insert(version_numbers, tonumber(number_str))
+	end
+	return version_numbers
+end
+
+
+
+--- Compare 2 version numbers, and return the order of the change (0 == no update, 1 == major, 2 == minor, -1 == behind)
+-- @param current The current version, as a list of the numbers in the version.
+-- @param current The expected version, as a list of the numbers in the version.
+-- @return 0 if the versions are the same, or the index of the number changed. A negative number if the current version is behind the expected one.
+local function CompareVersions(expected, current)
+	local order = 1
+	
+	while current[order] or expected[order] do
+		if (current[order] or 0) < (expected[order] or 0) then
+			return -order
+		elseif (current[order] or 0) > (expected[order] or 0) then
+			return order
+		else
+			order = order + 1
+		end
+	end
+	return 0
+end
+
+
+
+--- Compare 2 version number strings.
+-- @return (cf CompareVersions).
+local function CompareVersionStrings(expected, current)
+	return CompareVersions(StringToVersion(expected), StringToVersion(current))
+end
 
 
 
@@ -44,32 +83,22 @@ function pshy.version_Check()
 		print(string.format("<r>⚠ This script will not start after being %d days old.</r>", pshy.version_days_before_update_required))
 	end
 	-- check tfm api version
-	local expected_tfm_api_version_numbers = {}
-	for number_str in string.gmatch(__PSHY_TFM_API_VERSION__, "([^\.]+)") do
-		table.insert(expected_tfm_api_version_numbers, tonumber(number_str))
-	end
-	local current_tfm_api_version_numbers = {}
-	for number_str in string.gmatch(tfm.get.misc.apiVersion, "([^\.]+)") do
-		table.insert(current_tfm_api_version_numbers, tonumber(number_str))
-	end
-	if current_tfm_api_version_numbers[1] and expected_tfm_api_version_numbers[1] ~= current_tfm_api_version_numbers[1] then
+	local tfm_api_version_diff = CompareVersionStrings(__PSHY_TFM_API_VERSION__, tfm.get.misc.apiVersion)
+	if tfm_api_version_diff == 1 then
 		print("<o>⚠ The TFM LUA API had a major update, an update of pshy's script may be available for this new version.</o>")
-	elseif current_tfm_api_version_numbers[2] and expected_tfm_api_version_numbers[2] ~= current_tfm_api_version_numbers[2] then
+	elseif tfm_api_version_diff == 2 then
 		print("<j>⚠ The TFM LUA API had a minor update, an update of pshy's script may be available for this new version.</j>")
+	elseif tfm_api_version_diff < 0 then
+		print("<vi>⚠ The TFM LUA API version is behind what it is supposed to be</vi>")
 	end
 	-- check tfm version
-	local expected_tfm_version_numbers = {}
-	for number_str in string.gmatch(__PSHY_TFM_VERSION__, "([^\.]+)") do
-		table.insert(expected_tfm_version_numbers, tonumber(number_str))
-	end
-	local current_tfm_version_numbers = {}
-	for number_str in string.gmatch(tfm.get.misc.transformiceVersion, "([^\.]+)") do
-		table.insert(current_tfm_version_numbers, tonumber(number_str))
-	end
-	if current_tfm_version_numbers[1] and expected_tfm_version_numbers[1] ~= current_tfm_version_numbers[1] then
+	local tfm_version_diff = CompareVersionStrings(__PSHY_TFM_VERSION__, tfm.get.misc.transformiceVersion)
+	if tfm_version_diff == 1 then
 		print("<o>⚠ Transformice had a major update, an update of pshy's script may be available for this new version.</o>")
-	elseif current_tfm_version_numbers[2] and expected_tfm_version_numbers[2] ~= current_tfm_version_numbers[2] then
+	elseif tfm_version_diff == 2 then
 		print("<j>⚠ Transformice had a minor update, an update of pshy's script may be available for this new version.</j>")
+	elseif tfm_version_diff < 0 then
+		print("<vi>⚠ Transformice's version is behind what it is supposed to be!</vi>")
 	end
 end
 
