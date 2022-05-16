79a4a910f0bf90b76ea9282bc0777f9fd339b05a
Pshy0 <Pshy0@users.noreply.github.com>
2022-05-16 14:25:09 +0200

    added joining/leaving players

---
+++ lua/pshy/emulator/pshy_tfm_emulator_players.lua
@@ -0,0 +1,78 @@
+--- pshy_tfm_emulator_players.lua
+--
+-- Simulate players.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+--
+-- @require pshy_tfm_emulator_basic_environment.lua
+--
+-- @require_priority DEBUG
+pshy = pshy or {}
+
+
+
+--- Simulate a joining player.
+-- @note The table is recreated because so do TFM.
+function pshy.tfm_emulator_NewPlayer(joining_player_name, properties)
+	-- change the player map reference:
+	local old_player_map = tfm.get.room.playerList
+	new_player_map = {}
+	tfm.get.room.playerList = new_player_map
+	for player_name, player in pairs(old_player_map) do
+		new_player_map[player_name] = player
+	end
+	-- add the new player
+	new_player_map[joining_player_name] = {
+		cheeses = 0;
+		community = "int";
+		gender = 0;
+		hasCheese = false;
+		id = 0;
+		inHardMode = 0;
+		isDead = true;
+		isFacingRight = true;
+		isInvoking = false;
+		isJumping = false;
+		isShaman = false;
+		isVampire = false;
+		language = int;
+		look = "1;0,0,0,0,0,0,0,0,0";
+		movingLeft = false;
+		movingRight = false;
+		playerName = joining_player_name;
+		registrationDate = 1652691762994;
+		score = 0;
+		shamanMode = 0;
+		spouseId = 1;
+		spouseName = nil;
+		title = 0;
+		tribeId = nil;
+		tribeName = nil;
+		vx = 0;
+		vy = 0;
+		x = 0;
+		y = 0;
+	}
+	if properties then
+		local joining_player = new_player_map[joining_player_name]
+		for p_name, p_value in pairs(properties) do
+			joining_player[p_name] = p_value
+		end
+	end
+end
+
+
+
+--- Simulate a leaving player.
+-- @note The table is recreated because so do TFM.
+function pshy.tfm_emulator_PlayerLeft(leaving_player_name)
+	-- change the player map reference:
+	local old_player_map = tfm.get.room.playerList
+	new_player_map = {}
+	tfm.get.room.playerList = new_player_map
+	for player_name, player in pairs(old_player_map) do
+		new_player_map[player_name] = player
+	end
+	-- removing the player:
+	new_player_map[leaving_player_name] = nil
+end
