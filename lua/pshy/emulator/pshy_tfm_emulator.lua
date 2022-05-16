dc16f1501fcad95e19ae4f04e01b1fd38e4b489f
Pshy0 <Pshy0@users.noreply.github.com>
2022-05-16 15:16:29 +0200

    improved tests

--- lua/pshy/emulator/pshy_tfm_emulator.lua
+++ lua/pshy/emulator/pshy_tfm_emulator.lua
@@ -5,6 +5,7 @@
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 --
 -- @require pshy_tfm_emulator_basic_environment.lua
+-- @require pshy_tfm_emulator_game.lua
 -- @require pshy_tfm_emulator_players.lua
 -- @require pshy_tfm_emulator_time.lua
 --
@@ -17,12 +18,19 @@ pshy = pshy or {}
 function pshy.tfm_emulator_BasicTest()
 	-- Initialize room
 	pshy.tfm_emulator_init_NewPlayer("Pshy#3752")
+	pshy.tfm_emulator_init_NewPlayer("*invite_died")
+	pshy.tfm_emulator_init_NewPlayer("*invite_cheese")
+	pshy.tfm_emulator_init_NewPlayer("*invite_won")
 	pshy.tfm_emulator_NewPlayer("*Souris_65bc")
 	pshy.tfm_emulator_NewPlayer("*Souris_0000")
 	pshy.tfm_emulator_PlayerLeft("*Souris_65bc")
 	pshy.tfm_emulator_time_Add(500)
 	pshy.tfm_emulator_Loop(500, 199500)
 	pshy.tfm_emulator_NewPlayer("*Souris_65bc")
+	pshy.tfm_emulator_PlayerDied("*invite_died")
+	pshy.tfm_emulator_PlayerCheese("*invite_cheese")
+	pshy.tfm_emulator_PlayerCheese("*invite_won")
+	pshy.tfm_emulator_PlayerWon("*invite_won")
 	pshy.tfm_emulator_PlayerLeft("*Souris_65bc")
 	pshy.tfm_emulator_PlayerLeft("*Souris_0000")
 	pshy.tfm_emulator_time_Add(500)
