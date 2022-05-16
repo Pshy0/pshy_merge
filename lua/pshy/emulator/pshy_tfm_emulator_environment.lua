1fcb37004f49f31005ae03b5b37ee690206f8469
Pshy0 <Pshy0@users.noreply.github.com>
2022-05-16 21:15:44 +0200

    separate file for the emulator and its environment

---
+++ lua/pshy/emulator/pshy_tfm_emulator_environment.lua
@@ -0,0 +1,14 @@
+--- pshy_tfm_emulator.lua
+--
+-- Allow to emulate a TFM Lua module outside of TFM.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+--
+-- @require pshy_tfm_emulator_basic_environment.lua
+-- @require pshy_tfm_emulator_game.lua
+-- @require pshy_tfm_emulator_players.lua
+-- @require pshy_tfm_emulator_room.lua
+-- @require pshy_tfm_emulator_time.lua
+-- @require pshy_tfm_emulator_ui.lua
+--
+-- @require_priority DEBUG
