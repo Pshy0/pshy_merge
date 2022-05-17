3e72e3ee39f00965575b7e92f4d8ed1fb450e8bb
Pshy0 <Pshy0@users.noreply.github.com>
2022-05-17 21:50:32 +0200

    added pshy_motd

--- lua/pshy/games/pshy_pacmice.lua
+++ lua/pshy/games/pshy_pacmice.lua
@@ -23,6 +23,7 @@
 -- @require pshy_loopmore.lua
 -- @require pshy_mapdb.lua
 -- @require pshy_merge.lua
+-- @require pshy_motd.lua
 -- @require pshy_newgame.lua
 -- @require pshy_scores.lua
 -- @require pshy_splashscreen.lua
