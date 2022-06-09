710047c83943edb94b1e10dc75ceddefee532943
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-09 13:24:53 +0200

    added test file to test comments removal

---
+++ lua/test/README.md
@@ -0,0 +1,4 @@
+# Test
+
+This folder conatins scripts used to test pshy_merge.
+They are not intended for use in other scripts.
---
+++ lua/test/pshy_test_comments.lua
@@ -0,0 +1,38 @@
+-- comment 1
+not commented line 1
+-- comment 2
+not commented line 2
+-- comment 3
+-- comment 4
+not commented line 3
+-- comment 5 -- comment 6
+not commented line 4
+--comment 7
+not commented line 5
+---comment 7 bis
+not commented line 6
+ --comment 7 tris
+not commented line 7
+ ---comment 7 quadris
+not commented line 8
+not commented line 9 -- comment 8
+not commented line 10-- comment 9
+not commented line 11
+--[[ comment 10 ]] not commented end of line 12
+not commented line 13
+--[[ comment 11 ]]-- comment 12
+not commented line 14
+--[[ comment 11 ]]-- comment 13
+not commented line 15
+--[[
+	comment 14
+]]
+not commented line 16
+--[[
+	comment 15
+--]]
+not commented line 17
+start of empty lines
+
+
+end of empty lines
