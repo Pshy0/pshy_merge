b42e277f146415349e389f50418dc8b94c17a6b9
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-09 10:07:16 +0200

    added a third-party fix for grounds not seen by new players

---
+++ lua/pshy/patches/README.md
@@ -0,0 +1,4 @@
+# Patches
+
+This folder contains scripts supposed to fix issues or adding features to third-party scripts.
+They may not be compatible with all scripts.
---
+++ lua/pshy/patches/pshy_newplayergrounds.lua
@@ -0,0 +1,37 @@
+--- pshy_newplayergrounds.lua
+--
+-- Causes new players to see lua grounds
+--
+-- @author TFM:Pshy#3752 DC:PSHY#7998
+--
+-- @require pshy_merge.lua
+
+
+--- Internal Use:
+local grounds = {}
+
+
+
+local tfm_exec_addPhysicObject = tfm.exec.addPhysicObject
+tfm.exec.addPhysicObject = function(id, x, y, definition, ...)
+	grounds[id] = {x = x, y = y, definition = definition}
+	return tfm_exec_addPhysicObject(id, x, y, definition, ...)
+end
+
+
+
+local tfm_exec_removePhysicObject = tfm.exec.removePhysicObject
+tfm.exec.removePhysicObject = function(id)
+	grounds[id] = nil
+	return tfm.exec.removePhysicObject(id)
+end
+
+
+
+function eventNewPlayer(player_name)
+	for i_ground, ground in pairs(grounds) do
+		if not ground.definition.dynamic then
+			tfm_exec_addPhysicObject(i_ground, ground.x, ground.y, ground.definition)
+		end
+	end
+end
