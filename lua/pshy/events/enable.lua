94ac02f9920f2801afca492d998089a41388ff06
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-20 21:49:41 +0200

    added functions to enable or disable modules

---
+++ lua/pshy/events/enable.lua
@@ -0,0 +1,24 @@
+--- pshy.events.enable
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+pshy.require("pshy.events")
+
+
+
+--- Enable a module.
+local function EnableModule(module_name)
+	local module = pshy.modules[module_name]
+	if module.enabled == false then
+		module.enabled = true
+		for event_name, event in pairs(pshy.events) do
+			module_index = event.module_indices[module_name]
+			if module_index then
+				event.functions[module_index] = event.original_functions[module_index]
+			end
+		end
+	end
+end
+
+
+
+return EnableModule
