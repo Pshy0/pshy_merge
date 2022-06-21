a9d0b1814c6347661f2e21f968b2d7a87dbeeefe
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-21 21:11:38 +0200

    changed images to be lists

---
+++ lua/pshy/lists/images/bonuses.lua
@@ -0,0 +1,40 @@
+--- pshy.lists.images.bonuses
+--
+-- Bonus images.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998 (script)
+local images = pshy.require("pshy.lists.images")
+
+
+
+-- Bonuses (Pshy#3752)
+images["17bef4f49c5.png"] = {bonus = true, w = 30, h = 30, desc = "empty bonus"}
+images["17bf4b75aa7.png"] = {bonus = true, w = 30, h = 30, desc = "question bonus"}
+images["17bf4ba4ce5.png"] = {bonus = true, w = 30, h = 30, desc = "teleporter bonus"}
+images["17bf4b9e11d.png"] = {bonus = true, w = 30, h = 30, desc = "crate bonus"}
+images["17bf4b9af56.png"] = {bonus = true, w = 30, h = 30, desc = "high speed bonus"}
+images["17bf4b977f5.png"] = {bonus = true, w = 30, h = 30, desc = "ice cube bonus"}
+images["17bf4b94d8a.png"] = {bonus = true, w = 30, h = 30, desc = "snowflake bonus"}
+images["17bf4b91c35.png"] = {bonus = true, w = 30, h = 30, desc = "broken heart bonus"}
+images["17bf4b8f9e4.png"] = {bonus = true, w = 30, h = 30, desc = "heart bonus"}
+images["17bf4b8c42d.png"] = {bonus = true, w = 30, h = 30, desc = "feather bonus"}
+images["17bf4b89eba.png"] = {bonus = true, w = 30, h = 30, desc = "cross"}
+images["17bf4b868c3.png"] = {bonus = true, w = 30, h = 30, desc = "jumping mouse bonus"}
+images["17bf4b80fc3.png"] = {bonus = true, w = 30, h = 30, desc = "balloon bonus"}
+images["17bef4f49c5.png"] = {bonus = true, w = 30, h = 30, desc = "empty bonus"}
+images["17bf4b7ddd6.png"] = {bonus = true, w = 30, h = 30, desc = "triggered mouse trap"}
+images["17bf4b7a091.png"] = {bonus = true, w = 30, h = 30, desc = "mouse trap"}
+images["17bf4b7250e.png"] = {bonus = true, w = 30, h = 30, desc = "wings bonus"}
+images["17bf4b6f226.png"] = {bonus = true, w = 30, h = 30, desc = "transformations bonus"}
+images["17bf4b67579.png"] = {bonus = true, w = 30, h = 30, desc = "grow bonus"}
+images["17bf4b63aaa.png"] = {bonus = true, w = 30, h = 30, desc = "shrink bonus"}
+images["17bf4c421bb.png"] = {bonus = true, w = 30, h = 30, desc = "white flag bonus"}
+images["17e59dbef1e.png"] = {bonus = true, w = 30, h = 30, desc = "racing flag bonus"}
+images["17bf4f3f2fb.png"] = {bonus = true, w = 30, h = 30, desc = "v check"}
+images["17e53fb43dc.png"] = {bonus = true, w = 30, h = 30, desc = "cannonball bonus"}
+images["17e59ba43a6.png"] = {bonus = true, w = 30, h = 30, desc = "fish bonus"}
+images["17ebfdb85bd.png"] = {bonus = true, w = 30, h = 30, desc = "mouse skull bonus"}
+
+
+
+return images
---
+++ lua/pshy/lists/images/emoticons.lua
@@ -0,0 +1,38 @@
+--- pshy.lists.images.emoticons
+--
+-- Emoticon images.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998 (script)
+local images = pshy.require("pshy.lists.images")
+
+
+
+--- Emoticon images:
+images["16f56cbc4d7.png"] = {emoticon = true, w = 29, h = 26, desc = "nausea"}
+images["17088661168.png"] = {emoticon = true, w = 29, h = 26, desc = "cry"}
+images["16f5d8c7401.png"] = {emoticon = true, w = 29, h = 26, desc = "rogue"}
+images["16f56ce925e.png"] = {emoticon = true, desc = "happy cry"}
+images["16f56cdf28f.png"] = {emoticon = true, desc = "wonder"}
+images["16f56d09dc2.png"] = {emoticon = true, desc = "happy cry 2"}
+images["178ea94a353.png"] = {emoticon = true, w = 35, h = 30, desc = "vanlike novoice"}
+images["178ea9d3ff4.png"] = {emoticon = true, desc = "vanlike vomit"}
+images["178ea9d5bc3.png"] = {emoticon = true, desc = "vanlike big eyes"}
+images["178ea9d7876.png"] = {emoticon = true, desc = "vanlike pinklove"}
+images["178ea9d947c.png"] = {emoticon = true, desc = "vanlike eyelove"}
+images["178eac181f1.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 28, desc = "drawing zzz"}
+images["178ebdf194a.png"] = {emoticon = true, author = "rchl#0000", desc = "glasses1"}
+images["178ebdf317a.png"] = {emoticon = true, author = "rchl#0000", desc = "glasses2"}
+images["178ebdf0153.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 31, desc = "clown"}
+images["178ebdee617.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 31, desc = "vomit"}
+images["178ebdf495d.png"] = {emoticon = true, author = "rchl#0000", w = 35, h = 31, desc = "sad"}
+images["17aa125e853.png"] = {emoticon = true, author = "rchl#0000", w = 48, h = 48, desc = "sad2"}
+images["17aa1265ea4.png"] = {emoticon = true, author = "feverchild#0000", desc = "ZZZ"} -- source: https://discord.com/channels/246815328103825409/522398576706322454/834007372640419851
+images["17aa1264731.png"] = {emoticon = true, author = "feverchild#0000", desc = "no voice"}
+images["17aa1bcf1d4.png"] = {emoticon = true, author = "Nnaaaz#0000", w = 60, h = 60, desc = "pro"}
+images["17aa1bd3a05.png"] = {emoticon = true, author = "Nnaaaz#0000", w = 60, h = 49, desc = "noob"}
+images["17aa1bd0944.png"] = {emoticon = true, author = "Nnaaaz#0000", desc = "pro2"}
+images["17aa1bd20b5.png"] = {emoticon = true, author = "Nnaaaz#0000", desc = "noob2"}
+
+
+
+return images
