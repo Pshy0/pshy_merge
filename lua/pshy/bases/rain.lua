90892fb7b9ff515d74ef2986476e345672a9c0a0
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-23 19:59:16 +0200

    added a namespace for rain

--- lua/pshy/bases/rain.lua
+++ lua/pshy/bases/rain.lua
@@ -8,6 +8,11 @@ pshy.require("pshy.events")
 
 
 
+--- Namespace.
+local rain = {}
+
+
+
 --- Module's help page.
 pshy.help_pages["pshy_rain"] = {back = "pshy", title = "Object Rains", text = "Cause weird rains.", commands = {}}
 pshy.help_pages["pshy_rain"].commands = {}
@@ -16,35 +21,35 @@ pshy.help_pages["pshy"].subpages["pshy_rain"] = pshy.help_pages["pshy_rain"]
 
 
 --- Internal use:
-pshy.rain_enabled = false
-pshy.rain_next_drop_time = 0
-pshy.rain_object_types = {}
-pshy.rain_spawned_object_ids = {}
+rain.enabled = false
+rain.next_drop_time = 0
+rain.object_types = {}
+rain.spawned_object_ids = {}
 
 
 
 --- Random TFM objects.
 -- List of objects for random selection.
-pshy.rain_random_object_types = {}
-table.insert(pshy.rain_random_object_types, 1) -- little box
-table.insert(pshy.rain_random_object_types, 2) -- box
-table.insert(pshy.rain_random_object_types, 3) -- little board
-table.insert(pshy.rain_random_object_types, 6) -- ball
-table.insert(pshy.rain_random_object_types, 7) -- trampoline
-table.insert(pshy.rain_random_object_types, 10) -- anvil
-table.insert(pshy.rain_random_object_types, 17) -- cannon
-table.insert(pshy.rain_random_object_types, 33) -- chicken
-table.insert(pshy.rain_random_object_types, 39) -- apple
-table.insert(pshy.rain_random_object_types, 40) -- sheep
-table.insert(pshy.rain_random_object_types, 45) -- little board ice
-table.insert(pshy.rain_random_object_types, 54) -- ice cube
-table.insert(pshy.rain_random_object_types, 68) -- triangle
+rain.random_object_types = {}
+table.insert(rain.random_object_types, 1) -- little box
+table.insert(rain.random_object_types, 2) -- box
+table.insert(rain.random_object_types, 3) -- little board
+table.insert(rain.random_object_types, 6) -- ball
+table.insert(rain.random_object_types, 7) -- trampoline
+table.insert(rain.random_object_types, 10) -- anvil
+table.insert(rain.random_object_types, 17) -- cannon
+table.insert(rain.random_object_types, 33) -- chicken
+table.insert(rain.random_object_types, 39) -- apple
+table.insert(rain.random_object_types, 40) -- sheep
+table.insert(rain.random_object_types, 45) -- little board ice
+table.insert(rain.random_object_types, 54) -- ice cube
+table.insert(rain.random_object_types, 68) -- triangle
 
 
 
 --- Get a random TFM object.
 local function RandomTFMObjectType()
-	return pshy.rain_random_object_types[math.random(1, #pshy.rain_random_object_types)]
+	return rain.random_object_types[math.random(1, #rain.random_object_types)]
 end
 
 
@@ -58,20 +63,20 @@ end
 
 --- Drop an object in the sky when the rain is active.
 -- @private
-function pshy.rain_Drop()
+function rain.Drop()
 	if math.random(0, 1) == 0 then 
-		if pshy.rain_object_types == nil then
+		if rain.object_types == nil then
 			local new_id = SpawnRandomTFMObject()
-			table.insert(pshy.rain_spawned_object_ids, new_id)
+			table.insert(rain.spawned_object_ids, new_id)
 		else
-			local new_object_type = pshy.rain_object_types[math.random(#pshy.rain_object_types)]
+			local new_object_type = rain.object_types[math.random(#rain.object_types)]
 			assert(new_object_type ~= nil)
 			local new_id = SpawnRandomTFMObject(new_object_type)
-			table.insert(pshy.rain_spawned_object_ids, new_id)
+			table.insert(rain.spawned_object_ids, new_id)
 		end
 	end
-	if #pshy.rain_spawned_object_ids > 8 then
-		tfm.exec.removeObject(table.remove(pshy.rain_spawned_object_ids, 1))
+	if #rain.spawned_object_ids > 8 then
+		tfm.exec.removeObject(table.remove(rain.spawned_object_ids, 1))
 	end
 end
 
@@ -80,40 +85,40 @@ end
 --- Start the rain.
 -- @public
 -- @param types The object types/id to be summoning durring the rain.
-function pshy.rain_Start(types)
-	pshy.rain_enabled = true
-	pshy.rain_object_types = types
+function rain.Start(types)
+	rain.enabled = true
+	rain.object_types = types
 end
 
 
 
 --- Stop the rain.
 -- @public
-function pshy.rain_Stop()
-	pshy.rain_enabled = false
-	pshy.rain_object_types = nil
-	for i, id in ipairs(pshy.rain_spawned_object_ids) do
+function rain.Stop()
+	rain.enabled = false
+	rain.object_types = nil
+	for i, id in ipairs(rain.spawned_object_ids) do
 		tfm.exec.removeObject(id)
 	end
-	pshy.rain_spawned_object_ids = {}
+	rain.spawned_object_ids = {}
 end
 
 
 
 --- TFM event eventNewGame.
 function eventNewGame()
-	pshy.rain_next_drop_time = nil
+	rain.next_drop_time = nil
 end
 
 
 
 --- TFM event eventLoop.
 function eventLoop(time, time_remaining)
-	if pshy.rain_enabled then
-		pshy.rain_next_drop_time = pshy.rain_next_drop_time or time - 1
-		if pshy.rain_next_drop_time < time then
-			pshy.rain_next_drop_time = pshy.rain_next_drop_time + 500 -- run Tick() every 500 ms only
-			pshy.rain_Drop()
+	if rain.enabled then
+		rain.next_drop_time = rain.next_drop_time or time - 1
+		if rain.next_drop_time < time then
+			rain.next_drop_time = rain.next_drop_time + 500 -- run Tick() every 500 ms only
+			rain.Drop()
 		end
 	end
 end
@@ -124,15 +129,19 @@ end
 local function ChatCommandRain(user, ...)
 	rains_names = {...}
 	if #rains_names ~= 0 then
-		pshy.rain_Start(rains_names)
+		rain.Start(rains_names)
 		return true, "Rain started!"
-	elseif pshy.rain_enabled then
-		pshy.rain_Stop()
+	elseif rain.enabled then
+		rain.Stop()
 		return true, "Rain stopped!"
 	else
-	 	pshy.rain_Start(nil)
+	 	rain.Start(nil)
 	 	return true, "Random rain started!"
 	end
 end
 pshy.commands["rain"] = {perms = "admins", func = ChatCommandRain, desc = "start/stop an object/random object rain", argc_min = 0, argc_max = 4, arg_types = {tfm.enum.shamanObject, tfm.enum.shamanObject, tfm.enum.shamanObject, tfm.enum.shamanObject}, arg_names = {"shamanObject", "shamanObject", "shamanObject", "shamanObject"}}
 pshy.help_pages["pshy_rain"].commands["rain"] = pshy.commands["rain"]
+
+
+
+return rain
