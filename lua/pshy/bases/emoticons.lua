a5300b7e902811f20dbdcda72d46484b620ab693
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-23 20:34:31 +0200

    added a namespace for emoticons

--- lua/pshy/bases/emoticons.lua
+++ lua/pshy/bases/emoticons.lua
@@ -10,6 +10,11 @@ pshy.require("pshy.players")
 
 
 
+--- Namespace.
+local emoticons = {}
+
+
+
 --- Module Help Page:
 pshy.help_pages["pshy_emoticons"] = {back = "pshy", title = "Emoticons", text = "Adds custom emoticons\nUse the numpad numbers to use them. You may also use ALT or CTRL for more emoticons.\nIncludes emoticons from <ch>Nnaaaz#0000</ch>, <ch>Feverchild#0000</ch> and <ch>Rchl#3416</ch>\n", commands = {}}
 pshy.help_pages["pshy"].subpages["pshy_emoticons"] = pshy.help_pages["pshy_emoticons"]
@@ -21,82 +26,82 @@ perms.perms.everyone["emoticons"] = true	-- allow everybody to use emoticons
 local emoticons_delay = 256					-- minimum delay between custom emoticons
 local emoticons_mod1 = 16 					-- alternative emoji modifier key 1 (18 == ALT, SHIFT == 16)
 local emoticons_mod2 = 17 					-- alternative emoji modifier key 2 (17 == CTRL)
-pshy.emoticons = {}							-- list of available emoticons (image -> code, x/y -> top left location, sx/sy -> scale)
+emoticons.emoticons = {}							-- list of available emoticons (image -> code, x/y -> top left location, sx/sy -> scale)
 -- unknown author, https://atelier801.com/topic?f=6&t=894050&p=1#m16
-pshy.emoticons["unknown_vomit"]			= {image = "16f56cbc4d7.png", x = -15, y = -60} 
-pshy.emoticons["unknown_cry"]			= {image = "17088661168.png", x = -15, y = -60}
-pshy.emoticons["unknown_rogue"]			= {image = "16f5d8c7401.png", x = -15, y = -60}
-pshy.emoticons["unknown_happycry"]		= {image = "16f56ce925e.png", x = -15, y = -60}
-pshy.emoticons["unknown_wonder"]		= {image = "16f56cdf28f.png", x = -15, y = -60}
-pshy.emoticons["unknown_happycry2"]		= {image = "16f56d09dc2.png", x = -15, y = -60}
+emoticons.emoticons["unknown_vomit"]		= {image = "16f56cbc4d7.png", x = -15, y = -60} 
+emoticons.emoticons["unknown_cry"]			= {image = "17088661168.png", x = -15, y = -60}
+emoticons.emoticons["unknown_rogue"]		= {image = "16f5d8c7401.png", x = -15, y = -60}
+emoticons.emoticons["unknown_happycry"]		= {image = "16f56ce925e.png", x = -15, y = -60}
+emoticons.emoticons["unknown_wonder"]		= {image = "16f56cdf28f.png", x = -15, y = -60}
+emoticons.emoticons["unknown_happycry2"]	= {image = "16f56d09dc2.png", x = -15, y = -60}
 -- vanilla-like, unknown author
-pshy.emoticons["vanlike_novoice"]		= {image = "178ea94a353.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
-pshy.emoticons["vanlike_vomit"]			= {image = "178ea9d3ff4.png", x = -17, y = -61, sx = 0.92, sy = 0.92}
-pshy.emoticons["vanlike_bigeyes"]		= {image = "178ea9d5bc3.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
-pshy.emoticons["vanlike_pinklove"]		= {image = "178ea9d7876.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
-pshy.emoticons["vanlike_eyelove"]		= {image = "178ea9d947c.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
+emoticons.emoticons["vanlike_novoice"]		= {image = "178ea94a353.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
+emoticons.emoticons["vanlike_vomit"]		= {image = "178ea9d3ff4.png", x = -17, y = -61, sx = 0.92, sy = 0.92}
+emoticons.emoticons["vanlike_bigeyes"]		= {image = "178ea9d5bc3.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
+emoticons.emoticons["vanlike_pinklove"]		= {image = "178ea9d7876.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
+emoticons.emoticons["vanlike_eyelove"]		= {image = "178ea9d947c.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
 -- drawing, unknown author
-pshy.emoticons["drawing_zzz"]			= {image = "178eac181f1.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
+emoticons.emoticons["drawing_zzz"]			= {image = "178eac181f1.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
 -- rchl#0000, perm obtained
-pshy.emoticons["rchl_glasses1"]			= {image = "178ebdf194a.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-pshy.emoticons["rchl_glasses2"]			= {image = "178ebdf317a.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-pshy.emoticons["rchl_clown"]			= {image = "178ebdf0153.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-pshy.emoticons["rchl_sad"]				= {image = "178ebdf495d.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-pshy.emoticons["rchl_vomit"]			= {image = "178ebdee617.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-pshy.emoticons["rchl_sad2"]				= {image = "17aa125e853.png", x = -16, y = -62, sx = 0.65, sy = 0.65}
+emoticons.emoticons["rchl_glasses1"]		= {image = "178ebdf194a.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
+emoticons.emoticons["rchl_glasses2"]		= {image = "178ebdf317a.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
+emoticons.emoticons["rchl_clown"]			= {image = "178ebdf0153.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
+emoticons.emoticons["rchl_sad"]				= {image = "178ebdf495d.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
+emoticons.emoticons["rchl_vomit"]			= {image = "178ebdee617.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
+emoticons.emoticons["rchl_sad2"]			= {image = "17aa125e853.png", x = -16, y = -62, sx = 0.65, sy = 0.65}
 -- feverchild#0000, perm obtained, https://discord.com/channels/246815328103825409/522398576706322454/834007372640419851
-pshy.emoticons["feverchild_zzz"]		= {image = "17aa1265ea4.png", x = -17, y = -64, sx = 0.61, sy = 0.61}
-pshy.emoticons["feverchild_novoice"]	= {image = "17aa1264731.png", x = -17, y = -64, sx = 0.61, sy = 0.61}
+emoticons.emoticons["feverchild_zzz"]		= {image = "17aa1265ea4.png", x = -17, y = -64, sx = 0.61, sy = 0.61}
+emoticons.emoticons["feverchild_novoice"]	= {image = "17aa1264731.png", x = -17, y = -64, sx = 0.61, sy = 0.61}
 -- Nnaaaz#0000, request
-pshy.emoticons["pro"]					= {image = "17aa1bcf1d4.png", x = -20, y = -70, sx = 1, sy = 1, keep = true}
-pshy.emoticons["pro2"]					= {image = "17aa1bd0944.png", x = -20, y = -70, sx = 1, sy = 1, keep = true}
-pshy.emoticons["noob"]					= {image = "17aa1bd3a05.png", x = -30, y = -60, sx = 1, sy = 1, keep = true}
-pshy.emoticons["noob2"]					= {image = "17aa1bd20b5.png", x = -30, y = -60, sx = 1, sy = 1, keep = true}
-pshy.emoticons["cute"]					= {image = "17f7a2bf818.png", x = -35, y = -55, sx = 1, sy = 1, keep = true}
-pshy.emoticons["cute2"]					= {image = "17f7a2c9215.png", x = -35, y = -55, sx = 1, sy = 1, keep = true}
-pshy.emoticons["cutest"]				= {image = "17f7a2f6b53.png", x = -25, y = -55, sx = 1, sy = 1, keep = true}
+emoticons.emoticons["pro"]					= {image = "17aa1bcf1d4.png", x = -20, y = -70, sx = 1, sy = 1, keep = true}
+emoticons.emoticons["pro2"]					= {image = "17aa1bd0944.png", x = -20, y = -70, sx = 1, sy = 1, keep = true}
+emoticons.emoticons["noob"]					= {image = "17aa1bd3a05.png", x = -30, y = -60, sx = 1, sy = 1, keep = true}
+emoticons.emoticons["noob2"]				= {image = "17aa1bd20b5.png", x = -30, y = -60, sx = 1, sy = 1, keep = true}
+emoticons.emoticons["cute"]					= {image = "17f7a2bf818.png", x = -35, y = -55, sx = 1, sy = 1, keep = true}
+emoticons.emoticons["cute2"]				= {image = "17f7a2c9215.png", x = -35, y = -55, sx = 1, sy = 1, keep = true}
+emoticons.emoticons["cutest"]				= {image = "17f7a2f6b53.png", x = -25, y = -55, sx = 1, sy = 1, keep = true}
 -- other https://atelier801.com/topic?f=6&t=827044&p=1#m14
-pshy.emoticons["WTF_cat"]				= {image = "15565dbc655.png", x = -15, y = -65, sx = 0.75, sy = 0.75}
-pshy.emoticons["FUUU"]					= {image = "15568238225.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
-pshy.emoticons["me_gusta"]				= {image = "155682434d5.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
-pshy.emoticons["trollface"]				= {image = "1556824ac1a.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
-pshy.emoticons["cheese_right"]			= {image = "155592fd7d0.png", x = -15, y = -55, sx = 0.50, sy = 0.50}
-pshy.emoticons["cheese_left"]			= {image = "155593003fc.png", x = -15, y = -55, sx = 0.50, sy = 0.50}
+emoticons.emoticons["WTF_cat"]				= {image = "15565dbc655.png", x = -15, y = -65, sx = 0.75, sy = 0.75}
+emoticons.emoticons["FUUU"]					= {image = "15568238225.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
+emoticons.emoticons["me_gusta"]				= {image = "155682434d5.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
+emoticons.emoticons["trollface"]			= {image = "1556824ac1a.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
+emoticons.emoticons["cheese_right"]			= {image = "155592fd7d0.png", x = -15, y = -55, sx = 0.50, sy = 0.50}
+emoticons.emoticons["cheese_left"]			= {image = "155593003fc.png", x = -15, y = -55, sx = 0.50, sy = 0.50}
 -- unknown
-pshy.emoticons["mario_left"]			= {image = "156d7dafb2d.png", x = -25, y = -35, sx = 1, sy = 1, replace = true}
-pshy.emoticons["mario_right"]			= {image = "156d7dafb2d.png", x = 25, y = -35, sx = -1, sy = 1, replace = true}
+emoticons.emoticons["mario_left"]			= {image = "156d7dafb2d.png", x = -25, y = -35, sx = 1, sy = 1, replace = true}
+emoticons.emoticons["mario_right"]			= {image = "156d7dafb2d.png", x = 25, y = -35, sx = -1, sy = 1, replace = true}
 -- emoticons / index is (key_number + (100 * mod1) + (200 * mod2)) for up to 40 emoticons with only the numbers, ctrl and alt, including the defaults
-pshy.emoticons_binds = {}	
-pshy.emoticons_binds[101] = "vanlike_pinklove"
-pshy.emoticons_binds[102] = "unknown_cry"
-pshy.emoticons_binds[103] = "unknown_rogue"
-pshy.emoticons_binds[104] = "feverchild_zzz"
-pshy.emoticons_binds[105] = "unknown_happycry"
-pshy.emoticons_binds[106] = "trollface"
-pshy.emoticons_binds[107] = "unknown_wonder"
-pshy.emoticons_binds[108] = "rchl_sad2"
-pshy.emoticons_binds[109] = "unknown_happycry2"
-pshy.emoticons_binds[100] = "unknown_vomit"
-pshy.emoticons_binds[201] = "rchl_glasses1"
-pshy.emoticons_binds[202] = "rchl_sad"
-pshy.emoticons_binds[203] = "vanlike_bigeyes"
-pshy.emoticons_binds[204] = "rchl_glasses2"
-pshy.emoticons_binds[205] = "vanlike_eyelove"
-pshy.emoticons_binds[206] = "rchl_clown"
-pshy.emoticons_binds[207] = "vanlike_novoice"
-pshy.emoticons_binds[208] = "drawing_zzz"
-pshy.emoticons_binds[209] = "feverchild_novoice"
-pshy.emoticons_binds[200] = "rchl_vomit"
-pshy.emoticons_binds[301] = nil
-pshy.emoticons_binds[302] = nil
-pshy.emoticons_binds[303] = nil
-pshy.emoticons_binds[304] = "FUUU"
-pshy.emoticons_binds[305] = "me_gusta"
-pshy.emoticons_binds[306] = "trollface"
-pshy.emoticons_binds[307] = nil
-pshy.emoticons_binds[308] = "WTF_cat"
-pshy.emoticons_binds[309] = nil
-pshy.emoticons_binds[300] = "cheese"
+emoticons.binds = {}	
+emoticons.binds[101] = "vanlike_pinklove"
+emoticons.binds[102] = "unknown_cry"
+emoticons.binds[103] = "unknown_rogue"
+emoticons.binds[104] = "feverchild_zzz"
+emoticons.binds[105] = "unknown_happycry"
+emoticons.binds[106] = "trollface"
+emoticons.binds[107] = "unknown_wonder"
+emoticons.binds[108] = "rchl_sad2"
+emoticons.binds[109] = "unknown_happycry2"
+emoticons.binds[100] = "unknown_vomit"
+emoticons.binds[201] = "rchl_glasses1"
+emoticons.binds[202] = "rchl_sad"
+emoticons.binds[203] = "vanlike_bigeyes"
+emoticons.binds[204] = "rchl_glasses2"
+emoticons.binds[205] = "vanlike_eyelove"
+emoticons.binds[206] = "rchl_clown"
+emoticons.binds[207] = "vanlike_novoice"
+emoticons.binds[208] = "drawing_zzz"
+emoticons.binds[209] = "feverchild_novoice"
+emoticons.binds[200] = "rchl_vomit"
+emoticons.binds[301] = nil
+emoticons.binds[302] = nil
+emoticons.binds[303] = nil
+emoticons.binds[304] = "FUUU"
+emoticons.binds[305] = "me_gusta"
+emoticons.binds[306] = "trollface"
+emoticons.binds[307] = nil
+emoticons.binds[308] = "WTF_cat"
+emoticons.binds[309] = nil
+emoticons.binds[300] = "cheese"
 -- @todo 30 available slots in total :>
 
 
@@ -156,10 +161,10 @@ end
 --- Get an emoticon from name or bind index.
 local function EmoticonsGetEmoticon(emoticon)
 	if type(emoticon) == "number" then
-		emoticon = pshy.emoticons_binds[emoticon]
+		emoticon = emoticons.binds[emoticon]
 	end
 	if type(emoticon) == "string" then
-		emoticon = pshy.emoticons[emoticon]
+		emoticon = emoticons.emoticons[emoticon]
 	end
 	return emoticon
 end
@@ -292,10 +297,14 @@ pshy.help_pages["pshy_emoticons"].commands["emoticon"] = pshy.commands["emoticon
 --- !emoticons
 local function ChatCommandEmoticons(user)
 	tfm.exec.chatMessage("Available emoticons:", user)
-	for emoticon_name in pairs(pshy.emoticons) do
+	for emoticon_name in pairs(emoticons.emoticons) do
 		tfm.exec.chatMessage(string.format(" - %s", emoticon_name), user)
 	end 
 	return true
 end
 pshy.commands["emoticons"] = {perms = "admins", func = ChatCommandEmoticons, desc = "list hidden emoticons", argc_min = 0, argc_max = 0}
 pshy.help_pages["pshy_emoticons"].commands["emoticons"] = pshy.commands["emoticons"]
+
+
+
+return emoticons
