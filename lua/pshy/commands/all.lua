7665d266b697fc3228ab0f24e60aee26fece938b
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-23 16:06:45 +0200

    added pshy.command.all

---
+++ lua/pshy/commands/all.lua
@@ -0,0 +1,11 @@
+--- pshy.commands.all
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+pshy.require("pshy.commands.game")
+pshy.require("pshy.commands.help")
+pshy.require("pshy.commands.lua")
+pshy.require("pshy.commands.players")
+pshy.require("pshy.commands.room")
+pshy.require("pshy.commands.rp")
+pshy.require("pshy.commands.tfm")
+pshy.require("pshy.commands.tp")
