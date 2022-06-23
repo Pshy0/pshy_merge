8c90eb3010f62c36f4f2a5d4fb0a290aa88acc01
Pshy0 <Pshy0@users.noreply.github.com>
2022-06-23 20:04:03 +0200

    added a namespace for speedfly

--- lua/pshy/bonuses/speedfly.lua
+++ lua/pshy/bonuses/speedfly.lua
@@ -3,7 +3,7 @@
 -- Bonuses using the speedfly module.
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
-pshy.require("pshy.bases.speedfly")
+local speedfly = pshy.require("pshy.bases.speedfly")
 pshy.require("pshy.bonuses")
 pshy.require("pshy.events")
 pshy.require("pshy.lists.images.bonuses")
@@ -12,7 +12,7 @@ pshy.require("pshy.lists.images.bonuses")
 
 --- BonusFly.
 function pshy.bonuses_callback_BonusFly(player_name, bonus)
-	pshy.speedfly_Fly(player_name, 50)
+	speedfly.Fly(player_name, 50)
 end
 pshy.bonuses_types["BonusFly"] = {image = "17bf4b7250e.png", func = pshy.bonuses_callback_BonusFly}
 
@@ -20,6 +20,6 @@ pshy.bonuses_types["BonusFly"] = {image = "17bf4b7250e.png", func = pshy.bonuses
 
 --- BonusHighSpeed.
 function pshy.bonuses_callback_BonusHighSpeed(player_name, bonus)
-	pshy.speedfly_Speed(player_name, 200)
+	speedfly.Speed(player_name, 200)
 end
 pshy.bonuses_types["BonusHighSpeed"] = {image = "17bf4b9af56.png", func = pshy.bonuses_callback_BonusHighSpeed}
