865cd525bf7f26043cf66b04cd9c075deb5dee42
Pshy0 <Pshy0@users.noreply.github.com>
2022-07-01 13:27:58 +0200

    fixes and new commands

--- lua/pshy/commands/list/modules.lua
+++ lua/pshy/commands/list/modules.lua
@@ -21,8 +21,8 @@ function pshy.merge_ChatCommandModules(user, event_name)
 	tfm.exec.chatMessage("Modules (in require order):", user)
 	for i_module, module in pairs(pshy.modules_list) do
 		local status
-		if module.loaded == false then
-			status = "(<r>not loaded</r>)"
+		if not module.loaded then
+			status = "(<vi>not loaded</vi>)"
 		elseif module.event_count and module.event_count > 0 then
 			if module.enabled == false then
 				status = string.format("(%d <j>disabled</j> events)", module.event_count)
@@ -42,7 +42,6 @@ help_pages["pshy_commands_modules"].commands["modules"] = command_list["modules"
 
 --- !enablemodule
 function pshy.merge_ChatCommandModuleenable(user, mname)
-	tfm.exec.chatMessage("[Merge] Enabling " .. mname)
 	return EnableModule(mname)
 end
 command_list["enablemodule"] = {func = pshy.merge_ChatCommandModuleenable, desc = "enable a module (NOT SAFE)", argc_min = 1, argc_max = 1, arg_types = {"string"}}
@@ -52,7 +51,6 @@ help_pages["pshy_commands_modules"].commands["enablemodule"] = command_list["ena
 
 --- !disablemodule
 function pshy.merge_ChatCommandModuledisable(user, mname)
-	tfm.exec.chatMessage("[Merge] Disabling " .. mname)
 	return DisableModule(mname)
 end
 command_list["disablemodule"] = {func = pshy.merge_ChatCommandModuledisable, desc = "disable a module (NOT SAFE)", argc_min = 1, argc_max = 1, arg_types = {"string"}}
@@ -62,6 +60,8 @@ help_pages["pshy_commands_modules"].commands["disablemodule"] = command_list["di
 
 --- !modulestop
 local function ChatCommandModulestop(user)
+	print("<j>[Modules] </j>Stopping...")
+	tfm.exec.chatMessage("<j>[Modules] </j>Stopping...", user)
 	system.exit()
 end 
 command_list["modulestop"] = {perms = "admins", func = ChatCommandModulestop, desc = "stop the module", argc_min = 0, argc_max = 0}
@@ -71,7 +71,16 @@ help_pages["pshy_commands_modules"].commands["modulestop"] = command_list["modul
 
 --- !pshyversion
 local function ChatCommandPshyversion(user)
-	return true, string.format("Pshy repository version: %s", tostring(pshy.PSHY_VERSION))
+	return true, string.format("Pshy repository version: <r>%s</r>", tostring(pshy.PSHY_VERSION))
 end
-command_list["pshyversion"] = {aliases = {"version"}, perms = "everyone", func = ChatCommandPshyversion, desc = "Show the last repository version.", argc_min = 0, argc_max = 0}
+command_list["pshyversion"] = {perms = "everyone", func = ChatCommandPshyversion, desc = "show pshy_merge's repository version", argc_min = 0, argc_max = 0}
+help_pages["pshy_commands_modules"].commands["pshyversion"] = command_list["pshyversion"]
+
+
+
+--- !version
+local function ChatCommandScriptversion(user)
+	return true, string.format("Script repository version: <vp>%s</vp>", tostring(pshy.MAIN_VERSION or "Not repository version available."))
+end
+command_list["version"] = {perms = "everyone", func = ChatCommandScriptversion, desc = "show the current script's repository version", argc_min = 0, argc_max = 0}
 help_pages["pshy_commands_modules"].commands["pshyversion"] = command_list["pshyversion"]
