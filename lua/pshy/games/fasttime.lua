7b90dd5881dafb4f102fb1fceb53da064394bfd7
Pshy0 <Pshy0@users.noreply.github.com>
2022-07-25 17:20:44 +0200

    fixed missing anticheat components

--- lua/pshy/games/fasttime.lua
+++ lua/pshy/games/fasttime.lua
@@ -3,6 +3,8 @@
 -- @author TFM:Pshy#3752 DC:Pshy#7998 (script)
 pshy.require("pshy.alternatives.chat")
 pshy.require("pshy.anticheats.antiguest")
+pshy.require("pshy.anticheats.ban")
+pshy.require("pshy.anticheats.loadersync")
 pshy.require("pshy.bases.version")
 pshy.require("pshy.commands")
 local command_list = pshy.require("pshy.commands.list")
