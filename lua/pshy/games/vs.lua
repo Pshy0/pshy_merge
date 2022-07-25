b19d4166fa350a9a575f5de6eac58c2248318880
Pshy0 <Pshy0@users.noreply.github.com>
2022-07-25 17:20:44 +0200

    fixed missing anticheat components

--- lua/pshy/games/vs.lua
+++ lua/pshy/games/vs.lua
@@ -6,6 +6,7 @@
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 pshy.require("pshy.alternatives.chat")
+pshy.require("pshy.anticheats.ban")
 pshy.require("pshy.anticheats.loadersync")
 pshy.require("pshy.bases.lobby")
 pshy.require("pshy.bases.version")
