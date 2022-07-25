98438ef64555c4a69affc8eb0bc7b10e34a9a3e0
Pshy0 <Pshy0@users.noreply.github.com>
2022-07-25 16:19:58 +0200

    disabling the useless rp commands and addming back module ones

--- lua/pshy/commands/list/all.lua
+++ lua/pshy/commands/list/all.lua
@@ -3,8 +3,8 @@
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 pshy.require("pshy.commands.list.game")
 pshy.require("pshy.commands.list.lua")
+pshy.require("pshy.commands.list.modules")
 pshy.require("pshy.commands.list.players")
 pshy.require("pshy.commands.list.room")
-pshy.require("pshy.commands.list.rp")
 pshy.require("pshy.commands.list.tfm")
 pshy.require("pshy.commands.list.tp")
