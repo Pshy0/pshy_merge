503e407e8eb4a00ec0c82dfd94694f3292fba2bc
Pshy0 <Pshy0@users.noreply.github.com>
2022-08-02 00:04:10 +0200

    nah it's not needed

--- lua/pshy/games/123soleil.lua
+++ lua/pshy/games/123soleil.lua
@@ -4,7 +4,6 @@
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 pshy.require("pshy.alternatives.chat")
-pshy.require("pshy.alternatives.getplayersync")
 pshy.require("pshy.anticheats.loadersync")
 pshy.require("pshy.commands")
 local command_list = pshy.require("pshy.commands.list")
