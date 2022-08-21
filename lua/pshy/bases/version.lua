3e6e3f6377dce259c19b986b48132f9d46e31147
Pshy0 <Pshy0@users.noreply.github.com>
2022-08-21 19:52:43 +0200

    updated TFM version to v8.16

--- lua/pshy/bases/version.lua
+++ lua/pshy/bases/version.lua
@@ -10,7 +10,7 @@ local version = {}
 
 
 --- Module Settings:
-pshy.TFM_VERSION = "8.15"							-- The last tfm version this script was made for.
+pshy.TFM_VERSION = "8.16"							-- The last tfm version this script was made for.
 pshy.TFM_API_VERSION = "0.28"						-- The last tfm api version this script was made for.
 version.days_before_update_suggested = 14			-- How old the script should be before suggesting an update (`nil` to disable).
 version.days_before_update_advised = 30				-- How old the script should be before requesting an update (`nil` to disable).
