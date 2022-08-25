98b9894c2bebffa95487eefc24f85b7334e4e500
Pshy0 <Pshy0@users.noreply.github.com>
2022-08-25 14:44:38 +0200

    fixed module order

--- lua/pshy/bonuses/mapext.lua
+++ lua/pshy/bonuses/mapext.lua
@@ -6,6 +6,7 @@ local bonuses = pshy.require("pshy.bonuses")
 local bonus_types = pshy.require("pshy.bonuses.list")
 local map_list = pshy.require("pshy.maps.list")
 local newgame = pshy.require("pshy.rotations.newgame")
+pshy.require("pshy.bonuses.xmlext", false)
 
 
 
