b09d892ccfd4cbfc86872b36d0f61a47728708a4
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-05 13:53:26 +0100

    fixed events timing module

--- lua/pshy/debug/debugeventstiming.lua
+++ lua/pshy/debug/debugeventstiming.lua
@@ -16,6 +16,12 @@ local help_pages = pshy.require("pshy.help.pages")
 
 
 
+--- Module Help Page:
+help_pages["eventstiming"] = {back = "pshy", title = "Dbg Evnt Timing", commands = {}}
+help_pages["pshy"].subpages["eventstiming"] = help_pages["eventstiming"]
+
+
+
 --- Internal Use:
 local merge_debug_events = true
 local merge_debug_event_name = "eventKeyboard"
@@ -25,30 +31,30 @@ local merge_debug_event_name = "eventKeyboard"
 --- Create the event functions (debug timing variant).
 function CreateEventFuntionsTiming()
 	print("DEBUG: generating debug events")
-	assert(event_functions_created == false)
+	--assert(event_functions_created == false)
 	for event_name, event in pairs(events.events) do
 		local event_functions = event.functions
 		if not events.to_minimize[event_name] then
 			_ENV[event_name] = function(...)
 				-- Event functions's code
 				if merge_debug_events then
-					pshy_timing_Start(e_name)
+					pshy_timing_Start(event_name)
 				end
 				local rst
 				for i_func, func in ipairs(event_functions) do
-					if e_name == merge_debug_event_name then
-						pshy_timing_Start(e_name .. " " .. event_module_names[i_func])
+					if event_name == merge_debug_event_name then
+						pshy_timing_Start(event_name .. " " .. event_module_names[i_func])
 					end
 					rst = func(...)
-					if e_name == merge_debug_event_name then
-						pshy_timing_Stop(e_name .. " " .. event_module_names[i_func])
+					if event_name == merge_debug_event_name then
+						pshy_timing_Stop(event_name .. " " .. event_module_names[i_func])
 					end
 					if (rst ~= nil) then
 						break
 					end
 				end
 				if merge_debug_events then
-					pshy_timing_Stop(e_name)
+					pshy_timing_Stop(event_name)
 				end
 			end
 		end
@@ -67,7 +73,7 @@ local function ChatCommandEventstiming(user)
 	end
 end
 command_list["eventstiming"] = {func = ChatCommandEventstiming, desc = "Enable event timing (debug).", argc_min = 0, argc_max = 0}
-help_pages["pshy_merge"].commands["eventstiming"] = command_list["eventstiming"]
+help_pages["eventstiming"].commands["eventstiming"] = command_list["eventstiming"]
 
 
 
@@ -81,7 +87,7 @@ local function ChatCommandEventtiming(user, event_name)
 	end
 end
 command_list["eventtiming"] = {func = ChatCommandEventtiming, desc = "Enable event timing (debug).", argc_min = 0, argc_max = 1, arg_types = {"string"}, arg_names = {"event_name"}}
-help_pages["pshy_merge"].commands["eventtiming"] = command_list["eventtiming"]
+help_pages["eventstiming"].commands["eventtiming"] = command_list["eventtiming"]
 
 
 
@@ -90,7 +96,7 @@ local function ChatCommandEventstimingprint(user)
 	pshy.timing_PrintMeasures()
 end
 command_list["eventstimingprint"] = {func = ChatCommandEventstimingprint, desc = "Print event timing results.", argc_min = 0, argc_max = 0}
-help_pages["pshy_merge"].commands["eventstimingprint"] = command_list["eventstimingprint"]
+help_pages["eventstiming"].commands["eventstimingprint"] = command_list["eventstimingprint"]
 
 
 
@@ -99,7 +105,7 @@ local function ChatCommandEventstimingreset(user)
 	pshy.timing_ResetMeasures()
 end
 command_list["eventstimingreset"] = {func = ChatCommandEventstimingreset, desc = "Reset event timing.", argc_min = 0, argc_max = 0}
-help_pages["pshy_merge"].commands["eventstimingreset"] = command_list["eventstimingreset"]
+help_pages["eventstiming"].commands["eventstimingreset"] = command_list["eventstimingreset"]
 
 
 
