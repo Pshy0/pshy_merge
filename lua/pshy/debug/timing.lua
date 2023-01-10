84c066d0f1fb5d26497279f03859efe687fde29c
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-10 16:43:05 +0100

    using a namespace

--- lua/pshy/debug/timing.lua
+++ lua/pshy/debug/timing.lua
@@ -3,9 +3,9 @@
 -- Debug functions to measure time taken by functions.
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
-local utils_tables = pshy.require("pshy.utils.tables")
 local command_list = pshy.require("pshy.commands.list")
 local help_pages = pshy.require("pshy.help.pages")
+local utils_tables = pshy.require("pshy.utils.tables")
 
 
 
@@ -15,17 +15,22 @@ help_pages["pshy"].subpages["timing"] = help_pages["timing"]
 
 
 
+--- Namespace.
+local timing = {}
+
+
+
 --- Internal Use:
-pshy.timing_measures = {}
+local timing_measures = {}
 
 
 
 --- Start measuring a time.
-function pshy.timing_Start(name)
-	local measure = pshy.timing_measures[name]
+function timing.Start(name)
+	local measure = timing_measures[name]
 	if not measure then
-		pshy.timing_measures[name] = {}
-		measure = pshy.timing_measures[name]
+		timing_measures[name] = {}
+		measure = timing_measures[name]
 		measure.name = name
 		measure.total_time = 0
 		measure.start_count = 0
@@ -35,13 +40,13 @@ function pshy.timing_Start(name)
 	measure.total_count = measure.total_count + 1
 	measure.start_time = os.time()
 end
-pshy_timing_Start = pshy.timing_Start
+pshy_timing_Start = timing.Start
 
 
 
 --- Stop measuring a time.
-function pshy.timing_Stop(name)
-	local measure = pshy.timing_measures[name]
+function timing.Stop(name)
+	local measure = timing_measures[name]
 	assert(measure, "this measure does not exist")
 	assert(measure.start_count > 0, "this measure was not started")
 	measure.start_count = measure.start_count - 1
@@ -51,20 +56,20 @@ function pshy.timing_Stop(name)
 		measure.start_time = nil
 	end
 end
-pshy_timing_Stop = pshy.timing_Stop
+pshy_timing_Stop = timing.Stop
 
 
 
 --- Print all measures.
-function pshy.timing_PrintMeasures(user)
+function timing.PrintMeasures(user)
 	if not user then
 		print(string.format("<vi>Times at %u:</vi>", os.time()))
 	else
 		tfm.exec.chatMessage(string.format("<vi>Times at %u:</vi>", os.time()), user)
 	end
-	local keys = utils_tables.SortedKeys(pshy.timing_measures)
+	local keys = utils_tables.SortedKeys(timing_measures)
 	for i_measure, measure_name in ipairs(keys) do
-		local measure = pshy.timing_measures[measure_name]
+		local measure = timing_measures[measure_name]
 		local ms_per_call = string.sub(string.format("%.6f", measure.total_time / measure.total_count), 1, 8)
 		if not user then
 			print(string.format("<j>%s: <o>%dms</o> / <o>%dcalls</o> == <r>%s ms/call</r>", measure_name, measure.total_time, measure.total_count, ms_per_call))
@@ -78,8 +83,8 @@ end
 
 
 --- Reset measures.
-function pshy.timing_ResetMeasures()
-	for measure_name, measure in pairs(pshy.timing_measures) do
+function timing.ResetMeasures()
+	for measure_name, measure in pairs(timing_measures) do
 		measure.total_time = 0
 		measure.total_count = 0
 	end
@@ -90,7 +95,7 @@ end
 
 --- !debugtimingprint
 local function ChatCommandDebugtimingprint(user)
-	pshy.timing_PrintMeasures(user)
+	timing.PrintMeasures(user)
 	return true
 end
 command_list["debugtimingprint"] = {func = ChatCommandDebugtimingprint, desc = "Print event timing results.", argc_min = 0, argc_max = 0}
@@ -100,7 +105,11 @@ help_pages["timing"].commands["debugtimingprint"] = command_list["debugtimingpri
 
 --- !debugtimingreset
 local function ChatCommandDebugtimingreset(user)
-	pshy.timing_ResetMeasures()
+	timing.ResetMeasures()
 end
 command_list["debugtimingreset"] = {func = ChatCommandDebugtimingreset, desc = "Reset event timing.", argc_min = 0, argc_max = 0}
 help_pages["timing"].commands["debugtimingreset"] = command_list["debugtimingreset"]
+
+
+
+return timing
