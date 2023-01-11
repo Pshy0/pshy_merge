75c064d69ab5c2087134782d857ca4543a9bbf0e
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-11 12:31:38 +0100

    doc

--- lua/pshy/compiler/modules.lua
+++ lua/pshy/compiler/modules.lua
@@ -10,18 +10,23 @@ pshy = pshy or {}
 
 --- Module map.
 -- This map is filled by the compiler.
--- Keys are module names, values are tables.
--- Fields of each entry:
---	- name:				The name of the module.
---	- file:				The full file name of the module.
---	- loaded:			`true` when the module has already been loaded.
---	- load:				Function that loads the module.
---	- return:			What the module returned when it was loaded.	
---	- source:			A string whith the module's code.
---	- start_line:		The line at which this module is included in the compiled code.
---	- end_line:			Last line part of this module in the compiled code.
---	- locals:			Map of local accessors as defined in `./localwrapper/access.lua`.
---	- enable_count:		How many times this module was required to be enabled.
---	- required_modules:	Modules required by this one.
---	- manually_enabled:	If the module was enabled by the command-line or `!enablemodule`.
+-- Keys are module names, values are tables representing each module.
+-- Each of those tables have the followinf fields:
+-- Fields reserved for internal use:
+--	- load:						Function that loads the module.
+--	- return:					What the module returned when it was loaded.	
+-- Read-only fields:
+--	- name:						The name of the module.
+--	- file:						The full file name of the module.
+--	- loaded:					`true` when the module has already been loaded.
+--	- source:					A string whith the module's code.
+--	- start_line:				The line at which this module is included in the compiled code.
+--	- end_line:					Last line of this module in the compiled code.
+--	- locals:					Map of local accessors as defined in `./localwrapper/access.lua`.
+--	- enabled:					Is this module enabled.
+--	- enable_count:				How many times this module was required to be enabled.
+--	- required_modules:			Modules required by this one.
+--	- manually_enabled:			If the module was enabled by the command-line or `!enablemodule`.
+-- Fields that can be set before eventInit():
+--	- require_direct_enabling:	The module will not be enabled automatically as a dependency.
 pshy.modules = pshy.modules or {}
