20ce137c362be81688715f9cee93940cde82cdf3
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-14 15:32:59 +0100

    added a pshy.debug script to include most usefull debug features

---
+++ lua/pshy/debug/init.lua
@@ -0,0 +1,10 @@
+--- pshy.debug
+--
+-- Requires some debug modules.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998 (script)
+pshy.require("pshy.commands.list.lua")
+pshy.require("pshy.debug.assert")
+pshy.require("pshy.debug.glocals")
+pshy.require("pshy.debug.newglobals")
+pshy.require("pshy.debug.playerjoinleave")
