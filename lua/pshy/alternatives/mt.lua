56545bb46f9665cdaad1a9e4a114ea970f47f9df
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-25 06:20:38 +0100

    added two scripts for including fc or mt alternatives functionalities

--- lua/pshy/alternatives/mt.lua
+++ lua/pshy/alternatives/mt.lua
@@ -1,11 +1,13 @@
---- pshy.alternatives
+--- pshy.alternatives.mt
 --
 -- Allow some scripts using restricted lua features to still work when those are not available.
 --
--- This scripts requires everything needed to run a script with funcorp features.
+-- This scripts requires everything needed to run a module-team-only script.
 -- Scripts may still purposefully refuse to run.
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 pshy.require("pshy.alternatives.chat")
+pshy.require("pshy.alternatives.data")
 pshy.require("pshy.alternatives.getplayersync")
 pshy.require("pshy.alternatives.timers")
+pshy.require("pshy.alternatives.uniqueplayers")
