00c8dd70b17f38e102ac461fdfd25bdcae81755d
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-25 06:18:00 +0100

    added a script to simulate tfm.get.room.uniquePlayers

---
+++ lua/pshy/alternatives/uniqueplayers.lua
@@ -0,0 +1,40 @@
+--- pshy.alternatives.uniqueplayers
+--
+-- Replaces a module-team-only variable.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+pshy.require("pshy.events")
+
+
+
+local have_uniqueplayers_access = tfm.get.room.uniquePlayers ~= nil
+
+
+
+if not have_uniqueplayers_access then
+
+
+
+	local unique_players = 0
+	for player_name, player in pairs(tfm.get.room.playerList) do
+		unique_players = unique_players + 1
+	end
+	tfm.get.room.uniquePlayers = unique_players
+
+
+
+	function eventNewPlayer()
+		unique_players = unique_players + 1
+		tfm.get.room.uniquePlayers = unique_players
+	end
+
+
+
+	function eventPlayerLeft()
+		unique_players = unique_players - 1
+		tfm.get.room.uniquePlayers = unique_players
+	end
+	
+	
+	
+end
