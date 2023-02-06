aab9ef01f7f08dbc21961079c0c6d938fb4d49ea
Pshy0 <Pshy0@users.noreply.github.com>
2023-02-06 02:36:04 +0100

    splitted pshy.essentials_plus

--- lua/pshy/essentials/everything.lua
+++ lua/pshy/essentials/everything.lua
@@ -1,35 +1,34 @@
---- pshy.essentials_plus
+--- pshy.essentials.everything
 --
--- Useful optional modules.
+-- Requires as many scripts as possible.
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 pshy.require("pshy.alternatives.chat")
 pshy.require("pshy.alternatives.timers")
 pshy.require("pshy.anticheats.adminchat")
 pshy.require("pshy.anticheats.ban")
-pshy.require("pshy.bases.speedfly")
 pshy.require("pshy.bases.checkpoints")
 pshy.require("pshy.bases.emotes")
 pshy.require("pshy.bases.emoticons")
+pshy.require("pshy.bases.speedfly")
 pshy.require("pshy.bases.version")
 pshy.require("pshy.bases.rain")
 pshy.require("pshy.commands")
 pshy.require("pshy.commands.list.all")
-pshy.require("pshy.essentials")
+pshy.require("pshy.debug")
+pshy.require("pshy.help")
 pshy.require("pshy.images.changeimage")
 pshy.require("pshy.images.list.all")
 pshy.require("pshy.rotations.list.all")
 pshy.require("pshy.rotations.mapinfo")
 pshy.require("pshy.rotations.newgame")
-pshy.require("pshy.tools.bindmouse")
 pshy.require("pshy.tools.bindkey")
+pshy.require("pshy.tools.bindmouse")
 pshy.require("pshy.tools.entibot")
 pshy.require("pshy.tools.fcplatform")
 pshy.require("pshy.tools.getxml")
 pshy.require("pshy.tools.loadxml")
 pshy.require("pshy.tools.motd")
-pshy.require("pshy.utils.print")
-pshy.require("pshy.utils.rotation")
 pshy.require("pshy.utils.tfm_enum_fix")
 -- Adding the empty script last.
 pshy.require("pshy.debug.emptyscriptslot")
---
+++ lua/pshy/essentials/tribehouse.lua
@@ -0,0 +1,28 @@
+--- pshy.essentials.tribehouse
+--
+-- Useful modules in tribehouse.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+pshy.require("pshy.alternatives.chat")
+pshy.require("pshy.alternatives.timers")
+pshy.require("pshy.bases.checkpoints")
+pshy.require("pshy.bases.emotes")
+pshy.require("pshy.bases.emoticons")
+pshy.require("pshy.bases.speedfly")
+pshy.require("pshy.bases.version")
+pshy.require("pshy.bases.rain")
+pshy.require("pshy.commands")
+pshy.require("pshy.commands.list.all")
+pshy.require("pshy.help")
+pshy.require("pshy.images.changeimage")
+pshy.require("pshy.images.list.all")
+pshy.require("pshy.rotations.list.all")
+pshy.require("pshy.rotations.mapinfo")
+pshy.require("pshy.rotations.newgame")
+pshy.require("pshy.tools.bindkey")
+pshy.require("pshy.tools.bindmouse")
+pshy.require("pshy.tools.fcplatform")
+pshy.require("pshy.tools.getxml")
+pshy.require("pshy.tools.loadxml")
+-- Adding the empty script last.
+pshy.require("pshy.debug.emptyscriptslot")
