31db9837ec36c6e6df57c1f7a1047a6709ef9393
Pshy0 <Pshy0@users.noreply.github.com>
2023-02-08 17:21:29 +0100

    added --minify-locals

--- python/minifier.py
+++ python/minifier.py
@@ -4,6 +4,11 @@ import re
 
 
 
+LUA_SYMBOL_TOKENS = ["<=", ">=", "<", ">",  "==", "~=", "=", "[", "]", "(", ")", "{", "}", ";", "...", "..", ".", ":", ",", "+", "-", "*", "/", "^", "#", "%"]
+LUA_WORD_OPERATORS = ["and", "or", "not"]
+LUA_WORD_TOKENS = ["function", "while", "for", "do", "if", "elseif", "else", "then", "end", "local", "nil", "break", "repeat", "until", "true", "false", "in", "and", "or", "not", "return"]
+
+
 def GetStringOpenSequence(source, index):
     if source[index] == "\"":
         return "\""
@@ -63,6 +68,11 @@ def IsIdentifierChar(c):
 
 
 
+def IsNumberChar(c):
+    return c in "bx0123456789abcdefABCDEF."
+
+
+
 class StringToken:
     """ Code Token representing some text or comment. """
 
@@ -88,14 +98,14 @@ class StringToken:
 
 
 
-class CodeToken:
+class RawToken:
     """ Code Token representing executable code with no text or comment. """
     
     def __init__(self, code):
         self.m_code = code
     
     def Type(self):
-        return "code"
+        return "raw"
     
     def __str__(self):
         return self.m_code
@@ -132,6 +142,67 @@ class SpaceToken:
 
 
 
+class NumberToken:
+    """ Code Token representing a number. """
+    
+    def __init__(self, code):
+        assert(len(code) > 0)
+        self.m_code = code
+    
+    def Type(self):
+        return "number"
+    
+    def __str__(self):
+        return self.m_code
+
+
+
+class WordToken:
+    """ Code Token representing an identifier or keyword. """
+    
+    def __init__(self, code):
+        assert(len(code) > 0)
+        self.m_code = code
+        self.m_is_member = False
+        self.m_is_local = False
+        self.m_local_level = None
+    
+    def Type(self):
+        if self.m_code in LUA_WORD_TOKENS:
+            return "keyword"
+        return "identifier"
+    
+    def __str__(self):
+        return self.m_code
+
+
+
+class OperatorToken:
+    """ Code Token representing an operator. """
+    
+    def __init__(self, code):
+        self.m_code = code
+    
+    def Type(self):
+        return "operator"
+    
+    def __str__(self):
+        return self.m_code
+
+
+
+    
+def GetTokenStringsWithTypes(tokens):
+    strs = []
+    for token in tokens:
+        s = str(token) + "|" + token.Type()
+        if token.Type() == "identifier" and token.m_is_local:
+            s += "(local{})".format(token.m_scope_index)
+        strs.append(s)
+    return strs
+
+
+
 def TokenizeTexts(source):
     tokens = []
     token_start = 0
@@ -142,7 +213,7 @@ def TokenizeTexts(source):
         if open_sequence != None:
             i_open = i
             if i_after_last_sequence != i_open:
-                tokens.append(CodeToken(source[i_after_last_sequence : i_open]))
+                tokens.append(RawToken(source[i_after_last_sequence : i_open]))
             close_sequence = GetStringCloseSequence(open_sequence)
             i_close = -1
             i = i_open + len(open_sequence)
@@ -159,7 +230,7 @@ def TokenizeTexts(source):
         else:
             i += 1
     if i_after_last_sequence != i:
-        tokens.append(CodeToken(source[i_after_last_sequence : i]))
+        tokens.append(RawToken(source[i_after_last_sequence : i]))
     return tokens
 
 
@@ -167,7 +238,7 @@ def TokenizeTexts(source):
 def TokenizeSpaces(tokens):
     new_tokens = []
     for token in tokens:
-        if token.Type() == "code":
+        if token.Type() == "raw":
             text = token.m_code
             cur_is_space = text[0].isspace()
             cur_text = ""
@@ -178,21 +249,234 @@ def TokenizeSpaces(tokens):
                     if cur_is_space:
                         new_tokens.append(SpaceToken(cur_text))
                     else:
-                        new_tokens.append(CodeToken(cur_text))
+                        new_tokens.append(RawToken(cur_text))
                     if i < len(text):
                         cur_text = text[i]
                         cur_is_space = text[i].isspace()
         else:
             new_tokens.append(token) 
     return new_tokens
-    
+
+
+
+def TokenizeNumbers(tokens):
+    new_tokens = []
+    for token in tokens:
+        if token.Type() == "raw":
+            text = token.m_code
+            cur_is_number = (text[0] in "0123456789.")
+            cur_text = ""
+            for i in range(len(text) + 1):
+                is_number = cur_is_number
+                if i < len(text) and i > 0 and not cur_is_number:
+                    is_number = ((text[i] in "0123456789.") and not IsIdentifierChar(text[i - 1]))
+                if i < len(text) and i > 0 and cur_is_number:
+                    is_number = IsNumberChar(text[i])
+                if i < len(text) and cur_is_number == is_number:
+                    cur_text += text[i]
+                else:
+                    if cur_is_number:
+                        new_tokens.append(NumberToken(cur_text))
+                    else:
+                        new_tokens.append(RawToken(cur_text))
+                    if i < len(text):
+                        cur_text = text[i]
+                        cur_is_number = (text[i] in "0123456789.")
+        else:
+            new_tokens.append(token)
+    #print(GetTokenStringsWithTypes(new_tokens))
+    return new_tokens
+
+
+
+def GetOperator(source, i):
+    for op in LUA_SYMBOL_TOKENS:
+        if source.find(op, i, i + len(op)) == i:
+            return op
+    for str_op in LUA_WORD_OPERATORS:
+        if source.find(op, i, i + len(op)) == i and (len(source) - i <= len(str_op) or not IsIdentifierChar(source[i + len(str_op)])):
+            return str_op
+    return None
+
+
+
+def TokenizeOperators(tokens):
+    new_tokens = []
+    for token in tokens:
+        if token.Type() == "raw":
+            text = token.m_code
+            non_op_text = ""
+            i = 0
+            while i < len(text):
+                op = GetOperator(text, i)
+                if not op:
+                    non_op_text += text[i]
+                    i += 1
+                else:
+                    if len(non_op_text) > 0:
+                        new_tokens.append(RawToken(non_op_text))
+                        non_op_text = ""
+                    new_tokens.append(OperatorToken(op))
+                    i += len(op)
+            if len(non_op_text) > 0:
+                new_tokens.append(RawToken(non_op_text))
+                non_op_text = ""
+        else:
+            new_tokens.append(token) 
+    return new_tokens
+
+
+
+def TokenizeWords(tokens):
+    new_tokens = []
+    for token in tokens:
+        if token.Type() == "raw":
+            text = token.m_code
+            for c in text:
+                if not IsIdentifierChar(c):
+                    raise Exception(token)
+            new_tokens.append(WordToken(text))
+        else:
+            new_tokens.append(token) 
+    return new_tokens
+
+
+
+def TokenizationComputeScopes(tokens):
+    # TODO: this is an experiment
+    expect_dothen = False
+    accept_funcname = False
+    scope_index = 0
+    high_scope_index = 0
+    scope_level = 0
+    scopes = []
+    for token in tokens:
+        #print("SCOPE " + str(scope_level) + ":" + str(token))
+        #print("EXPECT DOTHEN: " + str(expect_dothen))
+        if token.Type() == "spaces":
+            continue
+        elif token.Type() == "identifier":
+            if accept_funcname:
+                token.m_scope_level = scope_level - 1
+                token.m_scope_index = scopes[-1]
+            else:
+                token.m_scope_level = scope_level
+                token.m_scope_index = scope_index
+            #print("IDENTIFIER")
+        else:
+            scope_mod = 0
+            if not str(token) in ".:":
+                accept_funcname = False
+            if str(token) in ["if", "while", "for"]:
+                scope_mod = 1
+                expect_dothen = True
+            elif str(token) in ["repeat", "function"]:
+                scope_mod = 1
+                if str(token) == "function":
+                    accept_funcname = True
+            elif str(token) in ["do", "then"]:
+                if not expect_dothen:
+                    scope_mod = 1
+                else:
+                    expect_dothen = False
+            elif str(token) in ["end", "until"]:
+                scope_mod = -1
+            elif str(token) in ["else", "elseif"]:
+                high_scope_index += 1
+                scope_index = high_scope_index
+            if scope_mod == 1:
+                scope_level += 1
+                scopes.append(scope_index)
+                high_scope_index += 1
+                scope_index = high_scope_index
+            elif scope_mod == -1:
+                scope_level -= 1
+                scope_index = scopes.pop()
+
 
 
 def Tokenize(source):
     tokens = TokenizeTexts(source)
     tokens = TokenizeSpaces(tokens)
+    tokens = TokenizeNumbers(tokens)
+    tokens = TokenizeOperators(tokens)
+    tokens = TokenizeWords(tokens)
+    TokenizationComputeScopes(tokens)
+    # finalize
+    were_member_access_op = False
+    local_def = False
+    local_just_def = False
+    for token in tokens:
+        if token.Type() == "comment" or token.Type() == "spaces":
+            continue
+        elif token.Type() == "raw":
+            raise Exception("Invalid token `{}`.".format(str(token)))
+        elif token.Type() == "operator":
+            assert(were_member_access_op == False)
+            if str(token) in ".:":
+                were_member_access_op = True
+            if str(token) == "," and local_just_def:
+                local_def = True
+                local_just_def = False
+            if str(token) in "=;":
+                local_just_def = False
+        elif str(token) == "local" or str(token) == "for":
+            assert(were_member_access_op == False)
+            assert(local_def == False)
+            local_def = True
+            local_just_def = False
+        elif token.Type() == "number":
+            assert(were_member_access_op == False)
+            assert(local_def == False)
+            local_just_def = False
+        elif token.Type() == "keyword":
+            assert(were_member_access_op == False)
+            if str(token) != "function":
+                local_def = False
+            local_just_def = False
+        elif token.Type() == "identifier":
+            if were_member_access_op:
+                token.m_is_member = True
+                were_member_access_op = False
+            elif local_def and token.Type() != "keyword":
+                assert(token.Type() != "number")
+                token.m_is_local = True
+                local_def = False
+                local_just_def = True
+            else:
+                local_just_def = False
     return tokens
 
+
+
+def NextCombination(identifier):
+    IDCHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if identifier == None:
+        return 'a'
+    else:
+        identifier = list(identifier)
+        i_c = 0
+        while True:
+            if i_c >= len(identifier):
+                identifier += IDCHARS[0]
+                return ''.join(identifier)
+            else:
+                c = identifier[i_c]
+                if c == IDCHARS[-1]:
+                    identifier[i_c] = IDCHARS[0]
+                    i_c += 1
+                else:
+                    i_idchar = IDCHARS.find(c)
+                    identifier[i_c] = IDCHARS[i_idchar + 1]
+                    return ''.join(identifier)
+
+
+
+def NextName(identifier):
+    identifier = NextCombination(identifier)
+    while identifier in LUA_WORD_TOKENS:
+        identifier = NextCombination(identifier)
+    return identifier
 
 
 class LUAMinifier:
@@ -204,6 +488,7 @@ class LUAMinifier:
         self.m_minify_unreadable = False
         self.m_minify_strings = False
         self.m_minify_strings_local_count = 120
+        self.m_minify_locals = False
         self.m_obfuscate = False
 
     def LoadModule(self, source):
@@ -215,7 +500,7 @@ class LUAMinifier:
             if token.Type() == "comment":
                 if i > 0:
                     prev_token = self.m_tokens[i - 1]
-                    if token.m_close_sequence == "\n" and prev_token.Type() == "code" and not prev_token.m_code.endswith('\n'):
+                    if token.m_close_sequence == "\n" and prev_token.Type() == "raw" and not prev_token.m_code.endswith('\n'):
                         prev_token.m_code += '\n'
                 self.m_tokens.pop(i)
 
@@ -239,6 +524,39 @@ class LUAMinifier:
             if str(token) == "":
                 self.m_tokens.pop(i)
 
+    def IdentifierExists(self, name):
+        for token in self.m_tokens:
+            if token.Type() == "identifier":
+                if str(token) == name:
+                    return True
+        return False
+
+    def MinifyLocals(self):
+        local_names = {}
+        for token in self.m_tokens:
+            if token.Type() == "identifier" and token.m_is_local and len(str(token)) > 2:
+                if not str(token) in local_names or token.m_scope_level < local_names[str(token)][0]:
+                    local_names[str(token)] = (token.m_scope_level, token.m_scope_index)
+        for token in self.m_tokens:
+            if token.Type() == "identifier":
+                for local_name in local_names:
+                    if local_name == str(token):
+                        if token.m_is_member or token.m_scope_level < local_names[local_name][0] or (token.m_scope_level == local_names[local_name][0] and token.m_scope_index != local_names[local_name][1]):
+                            local_names.pop(local_name)
+                            break
+        idname = None
+        new_local_names = {}
+        for local_name in local_names:
+            idname = NextName(idname)
+            while self.IdentifierExists(idname):
+                idname = NextName(idname)
+            new_local_names[local_name] = idname
+        #print("-- LOCALS: " + str(new_local_names), file=sys.stderr)
+        for token in self.m_tokens:
+            if token.Type() == "identifier":
+                if str(token) in new_local_names:
+                    token.m_code = new_local_names[str(token)]
+
     def MinifyStrings(self):
         strings = {}
         for token in self.m_tokens:
@@ -261,7 +579,7 @@ class LUAMinifier:
                     if token.Type() == "string":
                         st = str(token)
                         if st == s:
-                            self.m_tokens[i_token] = CodeToken("_" + str(s_number))
+                            self.m_tokens[i_token] = RawToken("_" + str(s_number))
                 if strs_names != "":
                     strs_names += ","
                     strs_texts += ","
@@ -270,9 +588,11 @@ class LUAMinifier:
             if s_number >= self.m_minify_strings_local_count:
                 break
         if strs_names != "":
-            self.m_tokens.insert(0, CodeToken("local " + strs_names + "=" + strs_texts)) #181666
+            self.m_tokens.insert(0, RawToken("local " + strs_names + "=" + strs_texts))
 
     def Minify(self):
+        if self.m_minify_locals:
+            self.MinifyLocals()
         if self.m_minify_strings:
             self.MinifyStrings()
         if self.m_minify_comments:
@@ -290,27 +610,37 @@ class LUAMinifier:
         if not source.endswith('\n'):
         	source += '\n'
         return source
-
-
+    
+    def GetTokenStrings(self):
+        strs = []
+        for token in self.m_tokens:
+            strs.append(str(token))
+        return strs
 
 def Main(argc, argv):
     m = LUAMinifier()
     m.m_minify_comments = True
     m.m_minify_spaces = True
+    m.m_minify_locals = True
     source = r"""
-local n
+local variable_nana = {}
 --- This is some module code
 -- it's not important
-function f()
-    a  =  5 -- why do i do that
-	b  =  "because\" i can"
-	 c  =  [[am i sure about that]]
-    print(tostring(a) .. b ..  c)
+local function faaa()
+    a = 4
+    global_aaaaaa  =  5.4 -- why do i do that
+        for i = 1, aaa do end
+        for i, v in ipairs(nana) do end
+	local local_bbbb  =  "because\" i can"
+	 global_c  =  [[am i sure about that]]
+    print(tostring(global_aaaaaa) .. local_bbbb ..  global_c)
+    variable_nana = variable_nana or 4
+    variable_nana.di = 4.6
 end
 --[[ multiline
  comment to
 serve you]]
- f()
+ faaa()
 return false
 """
     m.LoadModule(source)
@@ -318,6 +648,7 @@ return false
     print("minification done!")
     print("NEW SOURCE:")
     print(m.GetSource())
+    print(GetTokenStringsWithTypes(m.m_tokens))
 
 
 
