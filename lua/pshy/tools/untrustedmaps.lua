0d23a813fefca4ed4b6553fe5b88fef7aae08749
Pshy0 <Pshy0@users.noreply.github.com>
2023-02-13 14:09:39 +0100

    moved untrusted map warning features

---
+++ lua/pshy/tools/untrustedmaps.lua
@@ -0,0 +1,54 @@
+--- pshy.tools.untrustedmaps
+--
+-- Help knowing what untrusted maps were run by the script.
+--
+-- @author TFM:Pshy#3752 DC:Pshy#7998
+local command_list = pshy.require("pshy.commands.list")
+pshy.require("pshy.events")
+local help_pages = pshy.require("pshy.help.pages")
+pshy.require("pshy.utils.print")
+local perms = pshy.require("pshy.perms")
+local room = pshy.require("pshy.room")
+
+
+
+--- Namespace.
+local untrusted_map_list = {}
+local untrusted_map_set = {}
+
+
+
+--- Module Help Page:
+help_pages[__MODULE_NAME__] = {back = "pshy", title = "Untrusted Maps", restricted = true, text = "Help knowing what untrusted maps were run by the script.\n", commands = {}}
+help_pages["pshy"].subpages[__MODULE_NAME__] = help_pages[__MODULE_NAME__]
+
+
+
+function eventNewGame()
+	local trusted = perms.IsTrustedMap()
+	if not trusted and room.is_funcorp and tfm.get.room.xmlMapInfo then
+		print_warn("Untrusted map %s from %s.", tostring(tfm.get.room.currentMap), tfm.get.room.xmlMapInfo.author or "?")
+		if not untrusted_map_set[tfm.get.room.currentMap] then
+			untrusted_map_set[tfm.get.room.currentMap] = true
+			untrusted_map_list[#untrusted_map_list + 1] = tfm.get.room.currentMap
+		end
+	end
+end
+
+
+
+--- !untrustedmaps <page>
+local function ChatCommandUntrustedMaps(user, page)
+	page = page or 1
+	assert(page > 0)
+	maplist = ""
+	for i = (page - 1) * 40 + 1, (page - 1) * 40 + 40 + 1 do
+		if not untrusted_map_list[i] then
+			break
+		end
+		maplist = maplist .. tostring(untrusted_map_list[i]) .. "\n"
+	end
+	return true, string.format("Untrusted maps, page %d:\n%s", page, maplist)
+end
+command_list["untrustedmaps"] = {perms = "admins", func = ChatCommandUntrustedMaps, desc = "list untrusted maps run by the script", argc_min = 0, argc_max = 1, arg_types = {"number"}, arg_names = {"page"}}
+help_pages[__MODULE_NAME__].commands["untrustedmaps"] = command_list["untrustedmaps"]
