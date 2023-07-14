d284270126ff42e8f8256ad32d743a1dd8533a25
Pshy0 <Pshy0@users.noreply.github.com>
2023-07-14 19:47:57 +0200

    changed keys

--- lua/pshy/bases/emotes.lua
+++ lua/pshy/bases/emotes.lua
@@ -1,6 +1,6 @@
 --- pshy.bases.emotes
 --
--- Allow players to use some of the consumable emotes for free using F8-F12 keys.
+-- Allow players to use some of the consumable emotes for free using LEFTBRACKET, EQUALS, SEMICOLON, F11 and F12 keys.
 --
 -- @author TFM:Pshy#3752 DC:Pshy#7998
 pshy.require("pshy.events")
@@ -8,26 +8,26 @@ pshy.require("pshy.events")
 
 
 local function TouchPlayer(player_name)
-	system.bindKeyboard(player_name, 119, true, true)
-	system.bindKeyboard(player_name, 120, true, true)
-	system.bindKeyboard(player_name, 121, true, true)
 	system.bindKeyboard(player_name, 122, true, true)
 	system.bindKeyboard(player_name, 123, true, true)
+	system.bindKeyboard(player_name, 219, true, true)
+	system.bindKeyboard(player_name, 187, true, true)
+	system.bindKeyboard(player_name, 186, true, true)
 end
 
 
 
 function eventKeyboard(player_name, key_code)
-	if key_code >= 119 and key_code <= 123 then
-		if key_code == 119 then
+	if key_code >= 122 and key_code <= 219 then
+		if key_code == 122 then
 			tfm.exec.playEmote(player_name, tfm.enum.emote.marshmallow)
-		elseif key_code == 120 then
-			tfm.exec.playEmote(player_name, tfm.enum.emote.selfie)
-		elseif key_code == 121 then
-			tfm.exec.playEmote(player_name, tfm.enum.emote.partyhorn)
-		elseif key_code == 122 then
-			tfm.exec.playEmote(player_name, tfm.enum.emote.jigglypuff)
 		elseif key_code == 123 then
+			tfm.exec.playEmote(player_name, tfm.enum.emote.selfie)
+		elseif key_code == 219 then
+			tfm.exec.playEmote(player_name, tfm.enum.emote.partyhorn)
+		elseif key_code == 187 then
+			tfm.exec.playEmote(player_name, tfm.enum.emote.jigglypuff)
+		elseif key_code == 186 then
 			tfm.exec.playEmote(player_name, tfm.enum.emote.carnaval)
 		end
 	end
