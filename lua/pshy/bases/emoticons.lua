251373d5bc5276b4e72e83e306ed8aad31858add
Pshy0 <Pshy0@users.noreply.github.com>
2023-07-14 18:28:28 +0200

    reworked emoticon module

--- lua/pshy/bases/emoticons.lua
+++ lua/pshy/bases/emoticons.lua
@@ -6,6 +6,8 @@
 pshy.require("pshy.events")
 local help_pages = pshy.require("pshy.help.pages")
 local perms = pshy.require("pshy.perms")
+local keycodes = pshy.require("pshy.enums.keycodes")
+local utils_tables = pshy.require("pshy.utils.tables")
 
 
 
@@ -22,97 +24,88 @@ help_pages["pshy"].subpages[__MODULE_NAME__] = help_pages[__MODULE_NAME__]
 
 --- Module Settings:
 perms.perms.everyone["emoticons"] = true	-- allow everybody to use emoticons
-local emoticons_delay = 256					-- minimum delay between custom emoticons
-local emoticons_mod1 = 16 					-- alternative emoji modifier key 1 (18 == ALT, SHIFT == 16)
-local emoticons_mod2 = 17 					-- alternative emoji modifier key 2 (17 == CTRL)
-emoticons.emoticons = {}							-- list of available emoticons (image -> code, x/y -> top left location, sx/sy -> scale)
--- unknown author, https://atelier801.com/topic?f=6&t=894050&p=1#m16
-emoticons.emoticons["unknown_vomit"]		= {image = "16f56cbc4d7.png", x = -15, y = -60}
-emoticons.emoticons["unknown_cry"]			= {image = "17088661168.png", x = -15, y = -60}
-emoticons.emoticons["unknown_rogue"]		= {image = "16f5d8c7401.png", x = -15, y = -60}
-emoticons.emoticons["unknown_happycry"]		= {image = "16f56ce925e.png", x = -15, y = -60}
-emoticons.emoticons["unknown_wonder"]		= {image = "16f56cdf28f.png", x = -15, y = -60}
-emoticons.emoticons["unknown_happycry2"]	= {image = "16f56d09dc2.png", x = -15, y = -60}
--- vanilla-like, unknown author
-emoticons.emoticons["vanlike_novoice"]		= {image = "178ea94a353.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
-emoticons.emoticons["vanlike_vomit"]		= {image = "178ea9d3ff4.png", x = -17, y = -61, sx = 0.92, sy = 0.92}
-emoticons.emoticons["vanlike_bigeyes"]		= {image = "178ea9d5bc3.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
-emoticons.emoticons["vanlike_pinklove"]		= {image = "178ea9d7876.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
-emoticons.emoticons["vanlike_eyelove"]		= {image = "178ea9d947c.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
--- drawing, unknown author
-emoticons.emoticons["drawing_zzz"]			= {image = "178eac181f1.png", x = -16, y = -60, sx = 0.9, sy = 0.9}
--- rchl#0000, perm obtained
-emoticons.emoticons["rchl_glasses1"]		= {image = "178ebdf194a.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-emoticons.emoticons["rchl_glasses2"]		= {image = "178ebdf317a.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-emoticons.emoticons["rchl_clown"]			= {image = "178ebdf0153.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-emoticons.emoticons["rchl_sad"]				= {image = "178ebdf495d.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-emoticons.emoticons["rchl_vomit"]			= {image = "178ebdee617.png", x = -16, y = -62, sx = 0.9, sy = 0.9}
-emoticons.emoticons["rchl_sad2"]			= {image = "17aa125e853.png", x = -16, y = -62, sx = 0.65, sy = 0.65}
--- feverchild#0000, perm obtained, https://discord.com/channels/246815328103825409/522398576706322454/834007372640419851
-emoticons.emoticons["feverchild_zzz"]		= {image = "17aa1265ea4.png", x = -17, y = -64, sx = 0.61, sy = 0.61}
-emoticons.emoticons["feverchild_novoice"]	= {image = "17aa1264731.png", x = -17, y = -64, sx = 0.61, sy = 0.61}
--- Nnaaaz#0000, request
-emoticons.emoticons["pro"]					= {image = "17aa1bcf1d4.png", x = -20, y = -70, sx = 1, sy = 1, keep = true}
-emoticons.emoticons["pro2"]					= {image = "17aa1bd0944.png", x = -20, y = -70, sx = 1, sy = 1, keep = true}
-emoticons.emoticons["noob"]					= {image = "17aa1bd3a05.png", x = -30, y = -60, sx = 1, sy = 1, keep = true}
-emoticons.emoticons["noob2"]				= {image = "17aa1bd20b5.png", x = -30, y = -60, sx = 1, sy = 1, keep = true}
-emoticons.emoticons["cute"]					= {image = "17f7a2bf818.png", x = -35, y = -55, sx = 1, sy = 1, keep = true}
-emoticons.emoticons["cute2"]				= {image = "17f7a2c9215.png", x = -35, y = -55, sx = 1, sy = 1, keep = true}
-emoticons.emoticons["cutest"]				= {image = "17f7a2f6b53.png", x = -25, y = -55, sx = 1, sy = 1, keep = true}
--- other https://atelier801.com/topic?f=6&t=827044&p=1#m14
-emoticons.emoticons["WTF_cat"]				= {image = "15565dbc655.png", x = -15, y = -65, sx = 0.75, sy = 0.75}
-emoticons.emoticons["FUUU"]					= {image = "15568238225.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
-emoticons.emoticons["me_gusta"]				= {image = "155682434d5.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
-emoticons.emoticons["trollface"]			= {image = "1556824ac1a.png", x = -15, y = -60, sx = 0.75, sy = 0.75}
-emoticons.emoticons["cheese_right"]			= {image = "155592fd7d0.png", x = -15, y = -55, sx = 0.50, sy = 0.50}
-emoticons.emoticons["cheese_left"]			= {image = "155593003fc.png", x = -15, y = -55, sx = 0.50, sy = 0.50}
--- unknown
-emoticons.emoticons["mario_left"]			= {image = "156d7dafb2d.png", x = -25, y = -35, sx = 1, sy = 1, replace = true}
-emoticons.emoticons["mario_right"]			= {image = "156d7dafb2d.png", x = 25, y = -35, sx = -1, sy = 1, replace = true}
--- emoticons / index is (key_number + (100 * mod1) + (200 * mod2)) for up to 40 emoticons with only the numbers, ctrl and alt, including the defaults
-emoticons.binds = {}	
-emoticons.binds[101] = "vanlike_pinklove"
-emoticons.binds[102] = "unknown_cry"
-emoticons.binds[103] = "unknown_rogue"
-emoticons.binds[104] = "feverchild_zzz"
-emoticons.binds[105] = "unknown_happycry"
-emoticons.binds[106] = "trollface"
-emoticons.binds[107] = "unknown_wonder"
-emoticons.binds[108] = "rchl_sad2"
-emoticons.binds[109] = "unknown_happycry2"
-emoticons.binds[100] = "unknown_vomit"
-emoticons.binds[201] = "rchl_glasses1"
-emoticons.binds[202] = "rchl_sad"
-emoticons.binds[203] = "vanlike_bigeyes"
-emoticons.binds[204] = "rchl_glasses2"
-emoticons.binds[205] = "vanlike_eyelove"
-emoticons.binds[206] = "rchl_clown"
-emoticons.binds[207] = "vanlike_novoice"
-emoticons.binds[208] = "drawing_zzz"
-emoticons.binds[209] = "feverchild_novoice"
-emoticons.binds[200] = "rchl_vomit"
-emoticons.binds[301] = nil
-emoticons.binds[302] = nil
-emoticons.binds[303] = nil
-emoticons.binds[304] = "FUUU"
-emoticons.binds[305] = "me_gusta"
-emoticons.binds[306] = "trollface"
-emoticons.binds[307] = nil
-emoticons.binds[308] = "WTF_cat"
-emoticons.binds[309] = nil
-emoticons.binds[300] = "cheese"
--- @todo 30 available slots in total :>
+local emoticons_delay = 300					-- minimum delay between custom emoticons
+
+
+
+--- Emoticon dictionary image -> code, x/y -> top left location, sx/sy -> scale):
+emoticons.emoticons = {
+	-- vanilla
+	["vanilla_novoice"]		= {image = "178ea94a353.png", x = -15, y = -57, sx = 0.9, sy = 0.9},
+	["vanilla_vomit"]		= {image = "16f56cbc4d7.png", x = -14, y = -57},
+	["vanilla_pinklove"]	= {image = "178ea9d7876.png", x = -15, y = -57, sx = 0.9, sy = 0.9},
+	["vanilla_eyelove"]		= {image = "178ea9d947c.png", x = -15, y = -57, sx = 0.9, sy = 0.9},
+	["vanilla_bigeyes"]		= {image = "178ea9d5bc3.png", x = -15, y = -57, sx = 0.9, sy = 0.9},
+	-- from https://atelier801.com/topic?f=6&t=894050&p=1#m16
+	["cry"]					= {image = "17088661168.png", x = -14, y = -57},
+	["rogue"]				= {image = "16f5d8c7401.png", x = -14, y = -57},
+	["lol"]					= {image = "16f56ce925e.png", x = -14, y = -57},
+	["lol_2"]				= {image = "16f56d09dc2.png", x = -14, y = -57},
+	["wonder"]				= {image = "16f56cdf28f.png", x = -14, y = -57},
+	-- drawing, unknown author
+	["mouse_asleep"]		= {image = "178eac181f1.png", x = -15, y = -57, sx = 0.9, sy = 0.9},
+	-- rchl#0000, perm obtained
+	["glasses"]				= {image = "178ebdf194a.png", x = -15, y = -59, sx = 0.9, sy = 0.9}, -- based on vanilla emoticon
+	["glasses_2"]			= {image = "178ebdf317a.png", x = -15, y = -59, sx = 0.9, sy = 0.9}, -- based on vanilla emoticon
+	["clown"]				= {image = "178ebdf0153.png", x = -15, y = -59, sx = 0.9, sy = 0.9},
+	["pity_eyes"]			= {image = "178ebdf495d.png", x = -15, y = -59, sx = 0.9, sy = 0.9},
+	["vomit"]				= {image = "178ebdee617.png", x = -15, y = -59, sx = 0.9, sy = 0.9}, -- redundant with vanilla but better
+	["pity_legs"]			= {image = "17aa125e853.png", x = -15, y = -59, sx = 0.65, sy = 0.65}, -- redundant but good too
+	-- feverchild#0000, perm obtained, https://discord.com/channels/246815328103825409/522398576706322454/834007372640419851
+	["voiceless"]			= {image = "17aa1264731.png", x = -16, y = -61, sx = 0.61, sy = 0.61},
+	["asleep"]				= {image = "17aa1265ea4.png", x = -16, y = -61, sx = 0.61, sy = 0.61},
+	-- Nnaaaz#0000, request
+	["pro"]					= {image = "17aa1bcf1d4.png", x = -20, y = -70, sx = 1, sy = 1, reserved = true},
+	["pro2"]				= {image = "17aa1bd0944.png", x = -20, y = -70, sx = 1, sy = 1, reserved = true},
+	["noob"]				= {image = "17aa1bd3a05.png", x = -30, y = -60, sx = 1, sy = 1, reserved = true},
+	["noob2"]				= {image = "17aa1bd20b5.png", x = -30, y = -60, sx = 1, sy = 1, reserved = true},
+	["cute"]				= {image = "17f7a2bf818.png", x = -35, y = -55, sx = 1, sy = 1, reserved = true},
+	["cute2"]				= {image = "17f7a2c9215.png", x = -35, y = -55, sx = 1, sy = 1, reserved = true},
+	["cutest"]				= {image = "17f7a2f6b53.png", x = -25, y = -55, sx = 1, sy = 1, reserved = true},
+	-- other https://atelier801.com/topic?f=6&t=827044&p=1#m14
+	["WTF_cat"]				= {image = "15565dbc655.png", x = -15, y = -65, sx = 0.75, sy = 0.75},
+	["FUUU"]				= {image = "15568238225.png", x = -15, y = -60, sx = 0.75, sy = 0.75},
+	["me_gusta"]			= {image = "155682434d5.png", x = -15, y = -60, sx = 0.75, sy = 0.75},
+	["trollface"]			= {image = "1556824ac1a.png", x = -15, y = -60, sx = 0.75, sy = 0.75},
+	["cheese_right"]		= {image = "155592fd7d0.png", x = -15, y = -55, sx = 0.50, sy = 0.50},
+	["cheese_left"]			= {image = "155593003fc.png", x = -15, y = -55, sx = 0.50, sy = 0.50},
+}
+
+
+
+--- Key binds:
+local emoticon_key_order = {
+	keycodes.LEFTBRACKET,
+	keycodes.EQUALS,
+	keycodes.RIGHTBRACKET,
+	keycodes.SEMICOLON,
+	keycodes.GRAVE,
+	keycodes.BACKSLASH,
+	keycodes.COMMA,
+	keycodes.PERIOD,
+	keycodes.SLASH
+}
+local emoticon_default_binds = {
+	[keycodes.LEFTBRACKET] = "rogue",
+	[keycodes.EQUALS] = "wonder",
+	[keycodes.RIGHTBRACKET] = "glasses_2", -- TODO: determine whether this key is usable
+	[keycodes.SEMICOLON] = "lol",
+	[keycodes.GRAVE] = "cry",
+	[keycodes.BACKSLASH] = "voiceless",
+	[keycodes.COMMA] = "asleep",
+	[keycodes.PERIOD] = "vomit",
+	[keycodes.SLASH] = "pity_eyes"
+}
 
 
 
 -- Internal Use:
-local emoticons_players_mod2 = {}				-- shift keys state
-local emoticons_players_mod1 = {}				-- alt keys state
 local emoticons_last_loop_time = 0				-- last loop time
 local emoticons_players_image_ids = {}			-- the emote id started by the player
 local emoticons_players_emoticon = {}			-- the current emoticon of players
-local emoticons_players_end_times = {}			-- time at wich players started an emote / NOT DELETED
+local emoticons_players_end_times = {}			-- time at which players started an emote / NOT DELETED
 local emoticons_players_start_times = {}
+local emoticons_players_binds = {}
 
 
 
@@ -127,19 +120,10 @@ end
 
 --- Listen for a players modifiers:
 local function TouchPlayer(player_name)
-	system.bindKeyboard(player_name, emoticons_mod1, true, true)
-	system.bindKeyboard(player_name, emoticons_mod1, false, true)
-	system.bindKeyboard(player_name, emoticons_mod2, true, true)
-	system.bindKeyboard(player_name, emoticons_mod2, false, true)
-	--for number = 0, 9 do -- numbers
-	--	system.bindKeyboard(player_name, 48 + number, true, true)
-	--end
-	for number = 0, 9 do -- numpad numbers
-		system.bindKeyboard(player_name, 96 + number, true, true)
+	for key_code in pairs(emoticon_default_binds) do
+		system.bindKeyboard(player_name, key_code, true, true)
 	end
-	for number = 0, 9 do -- F1- F10
-		system.bindKeyboard(player_name, 112 + number, true, true)
-	end
+	emoticons_players_binds[player_name] = emoticon_default_binds
 	emoticons_players_start_times[player_name] = os.time()
 end
 
@@ -157,36 +141,14 @@ end
 
 
 
---- Get an emoticon from name or bind index.
-local function EmoticonsGetEmoticon(emoticon)
-	if type(emoticon) == "number" then
-		emoticon = emoticons.binds[emoticon]
-	end
-	if type(emoticon) == "string" then
-		emoticon = emoticons.emoticons[emoticon]
-	end
-	return emoticon
-end
-
-
-
 --- Play an emoticon over a player.
 -- Also removes the current one if being played.
 -- Does nothing if the emoticon is invalid
 -- @param player_name The name of the player.
--- @param emoticon Emoticon table, bind index, or name.
+-- @param emoticon Emoticon table.
 -- @param end_time Optional end time (relative to the current round).
 local function EmoticonsPlay(player_name, emoticon, end_time)
 	end_time = end_time or emoticons_last_loop_time + 4500
-	if type(emoticon) ~= "table" then
-		emoticon = EmoticonsGetEmoticon(emoticon)
-	end
-	if not emoticon then
-		if emoticons_players_emoticon[player_name] and not emoticons_players_emoticon[player_name].keep then
-			EmoticonsStop(player_name)
-		end
-		return
-	end
 	if emoticons_players_emoticon[player_name] ~= emoticon then
 		if emoticons_players_image_ids[player_name] then
 			tfm.exec.removeImage(emoticons_players_image_ids[player_name])
@@ -230,32 +192,19 @@ end
 
 function eventKeyboard(player_name, key_code, down, x, y)
 	if down then
-		--elseif key_code >= 48 and key_code < 58 then -- numbers
-		--	local index = (key_code - 48) + (emoticons_players_mod1[player_name] and 100 or 0) + (emoticons_players_mod2[player_name] and 200 or 0)
-		--	emoticons_players_emoticon[player_name] = nil -- todo sadly, native emoticons will always replace custom ones
-		--	EmoticonsPlay(player_name, index, emoticons_last_loop_time + 4500)
-		local emoticon_index
-		if key_code >= 112 and key_code < 122 then
-			emoticon_index = (key_code - 112) + (emoticons_players_mod2[player_name] and 200 or (emoticons_players_mod1[player_name] and 300 or 100))
-		elseif key_code >= 96 and key_code < 106 then
-			emoticon_index = (key_code - 96) + (emoticons_players_mod2[player_name] and 200 or (emoticons_players_mod1[player_name] and 300 or 100))
-		end
-		if emoticon_index then -- numpad numbers
-			if emoticons_players_start_times[player_name] + emoticons_delay > os.time() then
-				return false
+		if key_code >= 186 and key_code <= 220 then
+			local emoticon_name = emoticons_players_binds[player_name][key_code]
+			if emoticon_name then
+				if emoticons_players_start_times[player_name] + emoticons_delay > os.time() then
+					return false
+				end
+				if not perms.HavePerm(player_name, "emoticons") then
+					return
+				end
+				emoticons_players_emoticon[player_name] = nil -- todo sadly, native emoticons will always replace custom ones
+				EmoticonsPlay(player_name, emoticons.emoticons[emoticon_name])
 			end
-			if not perms.HavePerm(player_name, "emoticons") then
-				return
-			end
-			emoticons_players_emoticon[player_name] = nil -- todo sadly, native emoticons will always replace custom ones
-			EmoticonsPlay(player_name, emoticon_index, emoticons_last_loop_time + 4500)
-			return
 		end
-	end
-	if key_code == emoticons_mod1 then
-		emoticons_players_mod1[player_name] = down
-	elseif key_code == emoticons_mod2 then
-		emoticons_players_mod2[player_name] = down
 	end
 end
 
@@ -292,12 +241,17 @@ __MODULE__.commands = {
 			elseif not perms.HavePerm(user, "!emoticon-others") then
 				return false, "You are not allowed to use this command on others :c"
 			end
-			EmoticonsPlay(target, emoticon_name, emoticons_last_loop_time + 4500)
+			local emoticon = emoticons.emoticons[emoticon_name]
+			if emoticon.reserved and not perms.admins[user] then
+				return false, "This emoticon is reserved for use by room admins."
+			end
+			EmoticonsPlay(target, emoticon)
 			return true
 		end
 	},
-	["emoticons"] = {
-		perms = "admins",
+	["emoticonlist"] = {
+		aliases = {"emlist"},
+		perms = "everyone",
 		desc = "list hidden emoticons",
 		argc_min = 0,
 		argc_max = 0,
@@ -307,6 +261,33 @@ __MODULE__.commands = {
 				tfm.exec.chatMessage(string.format(" - %s", emoticon_name), user)
 			end
 			return true
+		end
+	},
+	["emoticonbind"] = {
+		aliases = {"embind"},
+		perms = "everyone",
+		desc = "set the emoticon used by one of the custom emoticon keys",
+		argc_min = 2,
+		argc_max = 2,
+		arg_types = {"number", "string"},
+		arg_names = {"bind number", "emoticon name"},
+		func = function(user, bind_number, emoticon_name)
+			local key_code = emoticon_key_order[bind_number]
+			if not key_code then
+				return false, string.format("The bind number must be between 1 and %d.", #emoticon_key_order)
+			end
+			local emoticon = emoticons.emoticons[emoticon_name]
+			if not emoticon then
+				return false, "Invalid emoticon. Type `!emlist` for a list of valid emoticons."
+			end
+			if emoticon.reserved and not perms.admins[user] then
+				return false, "This emoticon is reserved for use by room admins."
+			end
+			if emoticons_players_binds[user] == emoticon_default_binds then
+				emoticons_players_binds[user] = utils_tables.Copy(emoticon_default_binds)
+			end
+			emoticons_players_binds[user][key_code] = emoticon_name
+			return true, "Custom emoticon key bound!"
 		end
 	}
 }
