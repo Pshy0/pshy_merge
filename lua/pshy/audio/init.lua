d06af112cc43a40a63fa8b112bf64b55a80fa950
Pshy0 <Pshy0@users.noreply.github.com>
2023-08-28 16:11:25 +0200

    changed some sound commands

--- lua/pshy/audio/init.lua
+++ lua/pshy/audio/init.lua
@@ -30,7 +30,8 @@ local ambient_list = ambient_lib.ambient_list
 
 
 __MODULE__.commands = {
-	["sound"] = {
+	["playsound"] = {
+		aliases = {"play", "sound"},
 		perms = "admins",
 		desc = "Play a sound in the room. Can play sounds in parallel.",
 		argc_min = 1,
@@ -56,7 +57,7 @@ __MODULE__.commands = {
 	},
 	["musiccategories"] = {
 		perms = "admins",
-		desc = "List music categories you can une in place of music names.",
+		desc = "List music categories you can use in place of music names.",
 		argc_min = 0,
 		argc_max = 0,
 		func = function(user)
@@ -66,31 +67,41 @@ __MODULE__.commands = {
 			return true
 		end
 	},
-	["music"] = {
-		aliases = {"m"},
+	["loopmusic"] = {
+		aliases = {"music", "m"},
 		perms = "admins",
 		desc = "Play a music. Only one music may play at a time.",
 		argc_min = 1,
-		argc_max = 3,
-		arg_types = {"string", "number", "boolean"},
-		arg_names = {"sound path or music index", "volume (0-70-100)", "repeat"},
+		argc_max = 2,
+		arg_types = {"string", "number"},
+		arg_names = {"sound path or music index / stop", "volume (0-70-100)", "repeat"},
 		func = function(user, sound_name, volume, rep)
+			if sound_name == "stop" or sound_name == "off" then
+				tfm.exec.stopMusic("musique")
+				return true, "Music stopped"
+			end
+			local is_valid, msg = music_lib.IsValidMusic(sound_name)
+			if not is_valid then
+				return false, msg
+			end
+			tfm.exec.playMusic(sound_name, "musique", volume, true)
+			return true, string.format("Playing %s %s", msg, sound_name)
+		end
+	},
+	["playmusic"] = {
+		perms = "admins",
+		desc = "Play a music. Only one music may play at a time.",
+		argc_min = 1,
+		argc_max = 4,
+		arg_types = {"string", "number", "boolean", "boolean"},
+		arg_names = {"sound path or music index", "volume (0-70-100)", "repeat"},
+		func = function(user, sound_name, volume, rep, fade)
 			local is_valid, msg = music_lib.IsValidMusic(sound_name)
 			if not is_valid then
 				return false, msg
 			end
 			tfm.exec.playMusic(sound_name, "musique", volume, rep)
-			return true, string.format("Playing %s %s", msg, sound_name)
-		end
-	},
-	["stopmusic"] = {
-		perms = "admins",
-		desc = "Stops the music.",
-		argc_min = 0,
-		argc_max = 0,
-		func = function(user)
-			tfm.exec.stopMusic("musique")
-			return true, "Music stopped"
+			return true, string.format("Playing %s %s", msg, sound_name, fade)
 		end
 	},
 	["ambients"] = {
@@ -111,8 +122,12 @@ __MODULE__.commands = {
 		argc_min = 1,
 		argc_max = 2,
 		arg_types = {"string", "number"},
-		arg_names = {"sound path or ambient index", "volume (0-100)"},
+		arg_names = {"sound path or ambient index / stop", "volume (0-100)"},
 		func = function(user, sound_name, volume)
+			if sound_name == "stop" or sound_name == "off" then
+				tfm.exec.stopMusic("ambient")
+				return true, "Ambient sound stopped"
+			end
 			local index = tonumber(sound_name)
 			if index then
 				sound_name = ambient_list[index]
@@ -120,18 +135,8 @@ __MODULE__.commands = {
 					return false, string.format("Invalid ambient index. It must be between 1 and %d!", #ambient_list)
 				end
 			end
-			tfm.exec.playMusic(sound_name, 1, volume, true)
+			tfm.exec.playMusic(sound_name, "ambient", volume, true)
 			return true, string.format("Playing %s", sound_name)
-		end
-	},
-	["stopambient"] = {
-		perms = "admins",
-		desc = "Stops the ambient sound.",
-		argc_min = 0,
-		argc_max = 0,
-		func = function(user)
-			tfm.exec.stopMusic(1)
-			return true, "Ambient sound stopped"
 		end
 	}
 }
